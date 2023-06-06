5069571be49afc73caf2eb8204b64dd3e5cdb214
Yiheng He <he.yiheng@protonmail.com>
2023-06-06 15:12:11 +0800

    darktheme

--- private_dot_config/nvim/lua/plugins/markdown-preview.lua
+++ private_dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,10 @@
 return {
     "iamcco/markdown-preview.nvim",
     build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
-	config = function ()
-		vim.g.mkdp_browser = "/usr/bin/librewolf"
-	end,
+        vim.fn["mkdp#util#install"]()
+    end,
+    config = function ()
+        vim.g.mkdp_browser = "/usr/bin/librewolf"
+        vim.g.mkdp_theme = "dark"
+    end,
 }
-
