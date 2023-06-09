91218ebf69c214777ae9363bb4e85d07b127d987
Yiheng He <he.yiheng@protonmail.com>
2023-06-09 09:00:03 +0800

    added neorg export module

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -7,11 +7,13 @@ return {
             load = {
                 ["core.defaults"] = {}, -- Loads default behaviour
                 ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.export"] = {},
                 ["core.dirman"] = { -- Manages Neorg workspaces
                     config = {
                         workspaces = {
                             notes = "~/notes",
                         },
+                        default_workspace = "notes",
                     },
                 },
             },
