280d208e405f66f06249b2fe87695add01a8094c
Yiheng He <he.yiheng@protonmail.com>
2023-06-14 11:11:17 +0800

    better wallpaper name selection

--- dot_config/hypr/scripts/executable_wallpaper.sh
+++ dot_config/hypr/scripts/executable_wallpaper.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 wallpaper_folder=${HOME}/wallpapers
-wallpaper_path=$(ls -d ${wallpaper_folder}/* | wofi -d)
-echo ${wallpaper_path}
-swww img --transition-type any --transition-fps 60 ${wallpaper_path}
+wallpaper=$(ls ${wallpaper_folder} | wofi -d)
+swww img --transition-type any --transition-fps 60 ${wallpaper_folder}/${wallpaper}
