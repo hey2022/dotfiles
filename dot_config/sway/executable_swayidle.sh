4c29ec645da1b87d5f49e08923e8141a8ce49fdb
Yiheng He <he.yiheng@protonmail.com>
2023-09-27 09:54:58 +0800

    changed idle time

--- dot_config/sway/executable_swayidle.sh
+++ dot_config/sway/executable_swayidle.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-swayidle -w timeout 300 '~/.config/sway/swaylock.sh' \
-            timeout 600 'swaymsg "output * power off"' \
+swayidle -w timeout 600 '~/.config/sway/swaylock.sh' \
+            timeout 1800 'swaymsg "output * power off"' \
             resume 'swaymsg "output * power on"' \
             before-sleep '~/.config/sway/swaylock.sh'
