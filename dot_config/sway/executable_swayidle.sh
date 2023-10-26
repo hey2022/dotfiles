457267a70d9eb97e2d35b4c6bc24b8030c360041
Yiheng He <he.yiheng@protonmail.com>
2023-10-26 11:07:01 +0800

    Swaylock and Swayidle themed tokyo night
    Also moved config to individual file

--- dot_config/sway/executable_swayidle.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-swayidle -w timeout 600 '~/.config/sway/swaylock.sh' \
-            timeout 1800 'swaymsg "output * power off"' \
-            resume 'swaymsg "output * power on"' \
-            before-sleep '~/.config/sway/swaylock.sh'
