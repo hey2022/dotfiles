20e40d28c2392e9842c7681e20b891eb683cad57
Yiheng He <yiheng.he@proton.me>
2024-02-27 19:09:42 +0800

    doom: add ein

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -94,7 +94,7 @@
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        (lookup +dictionary)              ; navigate your code and its documentation
