fe41ba5460f76f44fa6dcc419a2de5d1b8de4186
Yiheng He <yiheng.he@proton.me>
2024-02-29 08:28:14 +0800

    script: better timings for inhibit sleep

--- dot_config/scripts/executable_media-inhibit-lid-suspend.sh
+++ dot_config/scripts/executable_media-inhibit-lid-suspend.sh
@@ -4,5 +4,5 @@ while true; do
     if playerctl status | grep -q "Playing"; then
         systemd-inhibit --what=handle-lid-switch --why="Prevent sleep while media is playing" sleep 10 &
     fi
-    sleep 5
+    sleep 9
 done
