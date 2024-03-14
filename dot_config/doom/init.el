cbcb84f227e8c4389d29e2f588579e739dad169a
Yiheng He <yiheng.he@proton.me>
2024-03-14 10:53:30 +0800

    feat(emacs): add chinese input

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -18,7 +18,7 @@
 
 (doom! :input
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
-       ;;chinese
+       chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
