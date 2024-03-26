b42f8a5cd47b3813c6a4d291fb7177ac6db05c0a
Yiheng He <yiheng.he@proton.me>
2024-03-26 22:23:23 +0800

    fix(inhibit suspend): multiple players
    Made the script work when there are multiple players at the same time.
    If one of them is paused then it will inhibit sleep.

--- dot_config/scripts/executable_media-inhibit-lid-suspend.sh
+++ dot_config/scripts/executable_media-inhibit-lid-suspend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 while true; do
-    if playerctl status | grep -q "Playing"; then
+    if playerctl --all-players status | grep -q "Playing"; then
         systemd-inhibit --what=handle-lid-switch --why="Prevent sleep while media is playing" sleep 10 &
     fi
     sleep 9.9
