4d7392ccbce9388361bebeb88f3c8323159d2099
Yiheng He <yiheng.he@proton.me>
2024-04-09 01:18:25 +0800

    feat(emacs): org added +hugo +journal +pomodoro

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -156,7 +156,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty +roam2 +dragndrop +gnuplot +present +pandoc)               ; organize your plain life in plain text
+       (org +pretty +roam2 +dragndrop +gnuplot +present +pandoc +hugo +journal +pomodoro)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
