580e903a91997fd3eee87cef5e9833ac90ee286f
Yiheng He <yiheng.he@proton.me>
2024-05-05 00:50:34 +0800

    chore(emacs): upgraded init.el

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -96,7 +96,6 @@
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        (lookup +dictionary)              ; navigate your code and its documentation
        lsp               ; M-x vscode
        (magit +forge)             ; a git porcelain for Emacs
@@ -112,7 +111,7 @@
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        tty               ; improve the terminal Emacs experience
 
        :lang
