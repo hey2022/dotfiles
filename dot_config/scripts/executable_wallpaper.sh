ffe06a4d74092c828aa4bf4d2aef508158c1c46f
Yiheng He <yiheng.he@proton.me>
2024-06-08 01:11:10 +0800

    fix(wallpaper): add "" around variables

--- dot_config/scripts/executable_wallpaper.sh
+++ dot_config/scripts/executable_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 wallpaper_folder=$HOME/wallpapers
-wallpaper_path=$wallpaper_folder/$(ls $wallpaper_folder | tofi)
-swww img --transition-type any --transition-fps 60 $wallpaper_path
+wallpaper_path=$wallpaper_folder/$(ls "$wallpaper_folder" | tofi)
+swww img --transition-type any --transition-fps 60 "$wallpaper_path"
