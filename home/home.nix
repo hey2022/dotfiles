35035ebd4da6898c77725e01833af7e3cfabd52f
Yiheng He <yiheng.he@proton.me>
2024-08-23 14:14:00 +0800

    feat(nix): add packages from laptop

--- home/home.nix
+++ home/home.nix
@@ -1,38 +1,56 @@
 { config, pkgs, ... }:
 
 {
-	home.username = "yiheng";
-	home.homeDirectory = "/home/yiheng";
+  home.username = "yiheng";
+  home.homeDirectory = "/home/yiheng";
 
-	# This value determines the Home Manager release that your
-	# configuration is compatible with. This helps avoid breakage
-	# when a new Home Manager release introduces backwards
-	# incompatible changes.
-	#
-	# You can update Home Manager without changing this value. See
-	# the Home Manager release notes for a list of state version
-	# changes in each release.
-	home.stateVersion = "24.05";
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
 
-	# Let Home Manager install and manage itself.
-	programs.home-manager.enable = true;
-	home.packages = with pkgs; [
-	        # Desktop
-		sway
-		waybar
-		swww
-		foot
-		wlogout
-		swaylock
-		swayidle
-		grim
-		slurp
-		wl-clipboard
-		mako
-		tofi
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  home.packages = with pkgs; [
+    # Desktop
+    sway
+    waybar
+    swww
+    foot
+    wlogout
+    swaylock
+    swayidle
+    grim
+    slurp
+    wl-clipboard
+    mako
+    tofi
 
-		# Apps
-		firefox
-		keepassxc
-	];
+    # Development
+    python311Packages.grip
+    bun
+    nixfmt
+    git
+    wget
+    neovim
+
+    # Research
+    zotero
+    biber
+
+    # CLI
+    trackma
+    fastfetch
+
+    # Apps
+    firefox
+    keepassxc
+    feishin
+    mullvad-vpn
+  ];
 }
