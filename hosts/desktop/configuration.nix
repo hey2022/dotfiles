1ae6b7d590d7994a6992c7cfca6c58ada395b8eb
Yiheng He <yiheng.he@proton.me>
2024-09-01 11:09:40 +0800

    feat(nix): allowUnfree

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -53,6 +53,7 @@
     extraGroups = [ "wheel" ];
   };
 
+  nixpkgs.config.allowUnfree = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
