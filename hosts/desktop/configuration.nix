d69e76a3e6d8f812dbb889c097a5bab73252c04c
Yiheng He <yiheng.he@proton.me>
2024-09-18 06:45:50 +0800

    feat(nix): enable nix-ld

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -9,6 +9,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../system/ly.nix
+    ../../system/nix-ld.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
