f9df5dda7cbdb1bf191dfbd53ee96173443562b9
Yiheng He <yiheng.he@proton.me>
2024-09-18 06:44:43 +0800

    feat(nix): add ly

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../system/ly.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
---
+++ system/ly.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  services.displayManager.ly = {
+    enable = true;
+  };
+}
