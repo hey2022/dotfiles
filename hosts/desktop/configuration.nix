c4534646bcd5bb2c80ef33c780d6d290d9e73fb1
Yiheng He <yiheng.he@proton.me>
2024-09-30 05:03:46 +0800

    feat: switch to greeter to regreet

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -9,7 +9,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../system/desktop/xdg
-    ../../system/ly.nix
+    ../../system/desktop/regreet.nix
     ../../system/nix-ld.nix
     ../../system/services
     ../../system/bluetooth.nix
---
+++ system/desktop/regreet.nix
@@ -0,0 +1,10 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  programs.regreet.enable = true;
+}
