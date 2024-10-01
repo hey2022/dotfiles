de08c051eaba4885002d53fc716443eb3acb4986
Yiheng He <yiheng.he@proton.me>
2024-10-01 23:46:22 +0800

    feat(emacs): add dirvish flag to dired

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -70,7 +70,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons +ranger)             ; making dired pretty [functional]
+       (dired +dirvish +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ibuffer         ; interactive buffer management
        (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
