e2b6f30152d708e5650f5d03032477bcd24fae83
Yiheng He <yiheng.he@proton.me>
2024-10-04 16:33:09 +0800

    feat(wezterm): add vim keybinds

--- home/desktop/terminal/wezterm/wezterm.lua
+++ home/desktop/terminal/wezterm/wezterm.lua
@@ -1,8 +1,25 @@
 local wezterm = require 'wezterm'
 
-local config = wezterm.config_builder()
+return {
+  front_end = "WebGpu",
 
-config.front_end = "WebGpu"
-config.default_prog = { 'nu' }
+  default_prog = { 'nu' },
 
-return config
+  keys = {
+    {key="n", mods="ALT", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+    {key="d", mods="ALT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+
+    {key="h", mods="ALT", action=wezterm.action{ActivateTabRelative=-1}},
+    {key="l", mods="ALT", action=wezterm.action{ActivateTabRelative=1}},
+    {key="1", mods="ALT", action=wezterm.action{ActivateTab=0}},
+    {key="2", mods="ALT", action=wezterm.action{ActivateTab=1}},
+    {key="3", mods="ALT", action=wezterm.action{ActivateTab=2}},
+    {key="4", mods="ALT", action=wezterm.action{ActivateTab=3}},
+    {key="5", mods="ALT", action=wezterm.action{ActivateTab=4}},
+    {key="6", mods="ALT", action=wezterm.action{ActivateTab=5}},
+    {key="7", mods="ALT", action=wezterm.action{ActivateTab=6}},
+    {key="8", mods="ALT", action=wezterm.action{ActivateTab=7}},
+    {key="9", mods="ALT", action=wezterm.action{ActivateTab=8}},
+    {key="0", mods="ALT", action=wezterm.action{ActivateTab=9}},
+  },
+}
