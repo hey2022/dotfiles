c57cefaffbca5291beffafa2c6786524447cfaba
Yiheng He <yiheng.he@proton.me>
2024-10-04 16:44:23 +0800

    feat(nushell): add aliases

--- home/shell/nushell/default.nix
+++ home/shell/nushell/default.nix
@@ -9,5 +9,16 @@
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
+    shellAliases = {
+      la = "ls -la";
+      ll = "ls -l";
+      cd = "z";
+      cat = "bat";
+      ec = "emacsclient -c";
+      ff = "fastfetch";
+
+      rm = ''echo "This is not the command you are looking for."; false'';
+      rmt = "trash-put";
+    };
   };
 }
