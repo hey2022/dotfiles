6ae953b4d9bde25b858964666e2b8ff59aae6135
Yiheng He <yiheng.he@proton.me>
2024-10-05 16:38:55 +0800

    feat(sway): add vnc intergration

--- home/desktop/vnc/default.nix
+++ home/desktop/vnc/default.nix
@@ -10,4 +10,11 @@
   home.packages = with pkgs; [
     tigervnc
   ];
+
+  wayland.windowManager.sway.extraConfig = lib.mkAfter ''
+    mode passthrough {
+        bindsym $mod+shift+p mode default; floating_modifier $mod normal
+    }
+    bindsym $mod+shift+p mode passthrough; floating_modifier none
+  '';
 }
