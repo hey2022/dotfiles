b9be2eb782e35c89f7ff37a8bd0b1fb3a2a4c16e
Yiheng He <yiheng.he@proton.me>
2024-10-06 00:01:53 +0800

    feat(sway): move force_kill into wkill.nix

---
+++ home/desktop/sway/wkill.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "wkill";
+      runtimeInputs = with pkgs; [
+        sway
+        jq
+        util-linux
+      ];
+      text = ''
+        PID=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true).pid')
+        kill -9 "$PID"
+      '';
+    })
+  ];
+}
