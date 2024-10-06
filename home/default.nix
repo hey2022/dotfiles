bc825cd042b0f9e3de654eacdb5c00a9e52e5db2
Yiheng He <yiheng.he@proton.me>
2024-10-06 18:08:47 +0800

    feat(nix): add flake to home-manager

--- home/default.nix
+++ home/default.nix
@@ -9,4 +9,11 @@
   imports = [ ./nixgl.nix ];
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 }
