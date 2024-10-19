49dd0de90e10de2ad359ecb25188c8bd9ac9d651
Yiheng He <yiheng.he@proton.me>
2024-10-19 14:37:15 +0800

    feat(nix): move emacs daemon to nix module

--- home/dev/emacs.nix
+++ home/dev/emacs.nix
@@ -7,6 +7,10 @@
     ripgrep
     fd
   ];
+  services.emacs = {
+    enable = true;
+    startWithUserSession = true;
+  };
   home.file = {
     ".config/doom" = {
       source = ../../.config/doom;
