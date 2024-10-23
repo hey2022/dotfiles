6253baab9f8c592a6a90b696d538dfbfc56da4cf
Yiheng He <yiheng.he@proton.me>
2024-10-23 17:45:46 +0800

    build: update nixGL home-manager module url

--- home/nixgl.nix
+++ home/nixgl.nix
@@ -1,15 +1,9 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}:
+{ lib, inputs, ... }:
 
 {
   imports = [
     (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/exzombie/home-manager-smona/refs/heads/nixgl-compat/modules/misc/nixgl.nix";
+      url = "https://raw.githubusercontent.com/Smona/home-manager/refs/heads/nixgl-compat/modules/misc/nixgl.nix";
       sha256 = "63d97d971edc98656a3ccde4df3aab278b15a25dbc58f1b91e1395a19ea22ccf";
     })
   ];
