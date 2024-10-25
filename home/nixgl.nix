8b4fcf57dbd991a7ec045e4caa3a8368414b4f0a
Yiheng He <yiheng.he@proton.me>
2024-10-25 17:50:24 +0800

    refactor: nixGL home-manager module merged

--- home/nixgl.nix
+++ home/nixgl.nix
@@ -1,12 +1,5 @@
 { lib, inputs, ... }:
 
 {
-  imports = [
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/Smona/home-manager/refs/heads/nixgl-compat/modules/misc/nixgl.nix";
-      sha256 = "63d97d971edc98656a3ccde4df3aab278b15a25dbc58f1b91e1395a19ea22ccf";
-    })
-  ];
-
   nixGL.packages = lib.mkIf (!builtins.pathExists "/etc/NIXOS") inputs.nixGL.packages;
 }
