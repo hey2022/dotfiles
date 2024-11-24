82cbd098374685939147d8175e07f4a9544d06ed
Yiheng He <yiheng.he@proton.me>
2024-11-24 16:46:43 +0800

    fix: NetworkManager-wait-online hanging
    www.github.com/NixOS/nixpkgs/issues/180175

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,4 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   imports = [./dnscrypt.nix];
   networking.networkmanager.enable = true;
+
+  # https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online = {
+    serviceConfig = {
+      ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
+    };
+  };
 }
