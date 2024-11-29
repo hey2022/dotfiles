5c0a3fc72787e45fb351faf8366374fd5cbefaa8
Yiheng He <yiheng.he@proton.me>
2024-11-30 04:02:30 +0800

    feat(nix): add nix-community cache

--- system/default.nix
+++ system/default.nix
@@ -5,9 +5,17 @@
     ./nix-ld.nix
   ];
   system.stateVersion = "23.05";
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   nixpkgs.config.allowUnfree = true;
 }
