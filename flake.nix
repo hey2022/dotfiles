b21ef1ca52c561b22735650105cd7dbf6d631a55
Yiheng He <yiheng.he@proton.me>
2024-11-30 03:42:28 +0800

    feat: setup emacs from emacs-overlay

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     stylix.url = "github:danth/stylix";
   };
 
--- home/dev/emacs.nix
+++ home/dev/emacs.nix
@@ -1,13 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  emacs = pkgs.emacs-unstable-pgtk;
+in {
   imports = [./tex.nix];
+  nixpkgs.overlays = [(import inputs.emacs-overlay)];
   home.packages = with pkgs; [
     git
-    emacs
     ripgrep
     fd
   ];
+  programs.emacs = {
+    enable = true;
+    package = emacs;
+    extraPackages = epkgs: [epkgs.vterm];
+  };
   services.emacs = {
     enable = true;
+    package = emacs;
     startWithUserSession = true;
   };
   home.file = {
