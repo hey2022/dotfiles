83a57c5ee8332bb9f1a94dd4c123903be2806e58
Yiheng He <yiheng.he@proton.me>
2025-01-03 04:25:38 +0800

    feat(git): default to rebase on pull

--- home/dev/git.nix
+++ home/dev/git.nix
@@ -9,7 +9,10 @@
       enable = true;
       userEmail = "yiheng.he@proton.me";
       userName = "Yiheng He";
-      extraConfig.init.defaultBranch = "main";
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+      };
       signing = {
         key = null;
         # signByDefault = true;
