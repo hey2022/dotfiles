6408672eed458121f86fe0f119d0fb14693b7b5f
Yiheng He <yiheng.he@proton.me>
2025-01-04 06:00:02 +0800

    feat(nix): added anki

--- home/profiles/education.nix
+++ home/profiles/education.nix
@@ -1,3 +1,3 @@
 {...}: {
-  imports = [../programs/email.nix ../programs/documents ../programs/inkscape.nix ../programs/dictionary.nix];
+  imports = [../programs/email.nix ../programs/documents ../programs/inkscape.nix ../programs/dictionary.nix ../programs/anki.nix];
 }
