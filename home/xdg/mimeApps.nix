5de2500551c3a03579a6d9fb26c6c388640329bf
Yiheng He <yiheng.he@proton.me>
2025-01-06 22:13:53 +0800

    feat: set default pdf reader to okular

--- home/xdg/mimeApps.nix
+++ home/xdg/mimeApps.nix
@@ -8,6 +8,8 @@
       "x-scheme-handler/https" = ["firefox.desktop"];
       "x-scheme-handler/about" = ["firefox.desktop"];
       "x-scheme-handler/unknown" = ["firefox.desktop"];
+
+      "application/pdf" = ["okular.desktop"];
     };
   };
 }
