1fec788f242af624112823c558bf7bdc65e57d7d
Yiheng He <yiheng.he@proton.me>
2025-01-12 05:05:19 +0800

    feat(DNS): improve dnscrypt configuration

--- system/network/dnscrypt.nix
+++ system/network/dnscrypt.nix
@@ -1,15 +1,21 @@
-{...}: {
+{pkgs, ...}: {
   networking = {
     networkmanager.dns = "none";
     nameservers = [
       "127.0.0.1"
       "::1"
     ];
-    hosts = {
-      "34.223.124.45" = ["neverssl.com"];
-    };
   };
   services.dnscrypt-proxy2 = {
     enable = true;
+    settings = {
+      server_names = ["mullvad-base-doh" "quad9-dnscrypt-ip6-filter-pri" "adguard-dns-ipv6" "cloudflare-security-ipv6" "dns0" "controld-block-malware-ad"];
+      forwarding_rules = builtins.toFile "forwarding-rules" ''
+        neverssl.com 192.168.1.1,$DHCP,$BOOTSTRAP
+      '';
+      blocked_names = {
+        blocked_names_file = "${pkgs.stevenblack-blocklist}/hosts";
+      };
+    };
   };
 }
