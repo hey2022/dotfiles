20e8f6207555946a9a864ca8337b0ea71529b892
Yiheng He <yiheng.he@proton.me>
2025-01-16 05:29:07 +0800

    feat: add zen browser

--- flake.nix
+++ flake.nix
@@ -40,6 +40,10 @@
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     tls-xb = {
       url = "github:hey2022/tls-xb";
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ home/programs/browsers/zen-browser.nix
@@ -0,0 +1,9 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    inputs.zen-browser.packages."${system}".default
+  ];
+}
