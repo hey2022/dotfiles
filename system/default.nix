7b6a55347d526e3e6dd9fc5de0ebf640e3612a48
Yiheng He <yiheng.he@proton.me>
2025-01-17 09:53:41 +0800

    refactor: move nix config to file

--- system/default.nix
+++ system/default.nix
@@ -1,33 +1,10 @@
 {...}: {
   imports = [
+    ./nix.nix
     ./network
     ./bluetooth.nix
     ./nix-ld.nix
     ./ssh.nix
   ];
   system.stateVersion = "24.11";
-  nix = {
-    settings = {
-      keep-going = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      substituters = [
-        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        "https://mirrors.ustc.edu.cn/nix-channels/store"
-        "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-    optimise.automatic = true;
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 30d";
-    };
-  };
-  nixpkgs.config.allowUnfree = true;
 }
