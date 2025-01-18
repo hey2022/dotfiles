604d1766395884df0224ed35443bf03208d6eb02
Yiheng He <yiheng.he@proton.me>
2025-01-18 08:16:54 +0800

    feat: theme rofi

--- home/desktop/rofi/default.nix
+++ home/desktop/rofi/default.nix
@@ -1,6 +1,11 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
   };
+  xdg.configFile."rofi".source = "${inputs.rofi-themes}/files";
 }
