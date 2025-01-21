70f7a9480c41ecfc71dd22de982d8cefb4baff6e
Yiheng He <yiheng.he@proton.me>
2025-01-21 22:43:30 +0800

    feat: improve steam compatibility

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -1,8 +1,15 @@
-{...}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    protontricks.enable = true;
+    gamescopeSession = {
+      enable = true;
+    };
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 }
