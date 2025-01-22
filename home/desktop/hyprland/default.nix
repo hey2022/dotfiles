ed0b08c78bd112ac6793af451e2b746e3cd37eaf
Yiheng He <yiheng.he@proton.me>
2025-01-22 17:14:56 +0800

    build: make hyprland use flake

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -21,6 +21,7 @@
   ];
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     systemd.enable = false;
   };
   home.packages = [
