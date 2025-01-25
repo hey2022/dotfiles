c4ce0ccaeb0d05091a5c6378058625bdc3da80be
Yiheng He <yiheng.he@proton.me>
2025-01-26 05:23:41 +0800

    refactor: move prism-launcher to own file

--- home/programs/games/minecraft/default.nix
+++ home/programs/games/minecraft/default.nix
@@ -1,9 +1,3 @@
-{pkgs, ...}: {
-  home.packages = [
-    (pkgs.prismlauncher.override {
-      prismlauncher-unwrapped = pkgs.prismlauncher-unwrapped.overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or []) ++ [./prism-crack.patch];
-      });
-    })
-  ];
+{...}: {
+  imports = [./prism-launcher.nix];
 }
---
+++ home/programs/games/minecraft/prism-launcher.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: {
+  home.packages = [
+    (pkgs.prismlauncher.override {
+      prismlauncher-unwrapped = pkgs.prismlauncher-unwrapped.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or []) ++ [./prism-crack.patch];
+      });
+    })
+  ];
+}
