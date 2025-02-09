d368fb76f4937e8bd4d4626a97f932e1e7332871
Yiheng He <yiheng.he@proton.me>
2025-02-09 23:26:46 +0800

    feat(media): add tmpfiles.rules

--- system/services/media/default.nix
+++ system/services/media/default.nix
@@ -1,4 +1,10 @@
 {...}: {
   imports = [./jellyfin.nix ./navidrome.nix ./torrent];
   users.groups.media = {};
+  systemd.tmpfiles.rules = [
+    "d /var/lib/media 775 root media -"
+    "d /var/lib/media/anime 775 root media - -"
+    "d /var/lib/media/music 775 root media - -"
+    "d /var/lib/media/downloads 775 root media - -"
+  ];
 }
