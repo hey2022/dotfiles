ae90d6644395ed7d9f7837b30968ec524e0956d6
Yiheng He <yiheng.he@proton.me>
2025-02-09 23:27:12 +0800

    fix: remove mitmproxy

--- home/dev/cyber-security.nix
+++ home/dev/cyber-security.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [mitmproxy rustscan];
+  home.packages = with pkgs; [rustscan];
   programs.firefox = lib.mkIf config.programs.firefox.enable {
     profiles.default = {
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
