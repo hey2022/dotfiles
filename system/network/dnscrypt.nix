5580d9750ec70b1dc2284762cf7df8188cc4a3d0
Yiheng He <yiheng.he@proton.me>
2025-02-17 16:50:43 +0800

    fix(dnscrypt): remove 192.168.1.1 from forwarding

--- system/network/dnscrypt.nix
+++ system/network/dnscrypt.nix
@@ -11,7 +11,7 @@
     settings = {
       server_names = ["mullvad-base-doh" "quad9-dnscrypt-ip6-filter-pri" "adguard-dns-ipv6" "cloudflare-security-ipv6" "dns0" "controld-block-malware-ad"];
       forwarding_rules = builtins.toFile "forwarding-rules" ''
-        neverssl.com 192.168.1.1,$DHCP,$BOOTSTRAP
+        neverssl.com $DHCP,$BOOTSTRAP
       '';
       blocked_names = {
         blocked_names_file = "${pkgs.stevenblack-blocklist}/hosts";
