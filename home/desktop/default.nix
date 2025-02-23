cf7af0ab418ea36eca6bd7e27378da56c4671dc3
Yiheng He <yiheng.he@proton.me>
2025-02-24 05:35:11 +0800

    fix(wluma): use upstream module

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -3,36 +3,4 @@
   home.packages = with pkgs; [
     desktop-file-utils
   ];
-  services.wluma = {
-    enable = true;
-    settings = {
-      als.time = {
-        thresholds = {
-          "0" = "night";
-          "7" = "dim";
-          "8" = "normal";
-          "11" = "bright";
-          "13" = "normal";
-          "18" = "dim";
-          "21" = "dark";
-          "23" = "night";
-        };
-      };
-
-      output.backlight = [
-        {
-          name = "eDP-1";
-          path = "/sys/class/backlight/intel_backlight";
-          capturer = "wayland";
-        }
-      ];
-
-      keyboard = [
-        {
-          name = "keyboard-thinkpad";
-          path = "/sys/bus/platform/devices/thinkpad_acpi/leds/tpacpi::kbd_backlight";
-        }
-      ];
-    };
-  };
 }
--- home/desktop/wluma.nix
+++ home/desktop/wluma.nix
@@ -1,47 +1,34 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-with lib; let
-  cfg = config.services.wluma;
-in {
-  options.services.wluma = {
-    enable = mkEnableOption "Automatic brightness adjustment based on screen contents and ALS";
-
-    settings = mkOption {
-      type = types.attrs;
-      default = {};
-      description = ''
-        Configuration for wluma, in Nix format.
-        Will be converted to TOML syntax.
-        See https://github.com/maximbaz/wluma for configuration options.
-      '';
-    };
-  };
-
-  config = mkIf cfg.enable {
-    home.packages = [pkgs.wluma];
-
-    xdg.configFile."wluma/config.toml".source = pkgs.writers.writeTOML "wluma-config" cfg.settings;
-
-    systemd.user.services.wluma = {
-      Unit = {
-        Description = "Adjusting screen brightness based on screen contents and amount of ambient light";
-        PartOf = ["graphical-session.target"];
-        After = ["graphical-session.target"];
+{...}: {
+  services.wluma = {
+    enable = true;
+    settings = {
+      als.time = {
+        thresholds = {
+          "0" = "night";
+          "7" = "dim";
+          "8" = "normal";
+          "11" = "bright";
+          "13" = "normal";
+          "18" = "dim";
+          "21" = "dark";
+          "23" = "night";
+        };
       };
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
-      Service = {
-        ExecStart = "${pkgs.wluma}/bin/wluma";
-        Restart = "always";
-        EnvironmentFile = "-%E/wluma/service.conf";
-        PrivateNetwork = true;
-        PrivateMounts = false;
-      };
+
+      output.backlight = [
+        {
+          name = "eDP-1";
+          path = "/sys/class/backlight/intel_backlight";
+          capturer = "wayland";
+        }
+      ];
+
+      keyboard = [
+        {
+          name = "keyboard-thinkpad";
+          path = "/sys/bus/platform/devices/thinkpad_acpi/leds/tpacpi::kbd_backlight";
+        }
+      ];
     };
   };
 }
