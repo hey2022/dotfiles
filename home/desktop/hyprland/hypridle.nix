dfb5f326780d42ac98138c5a67a91ba631769330
Yiheng He <yiheng.he@proton.me>
2025-03-02 11:10:46 +0800

    hypridle: only put suspend behind laptop config

--- home/desktop/hyprland/hypridle.nix
+++ home/desktop/hyprland/hypridle.nix
@@ -4,32 +4,31 @@
   ...
 }: {
   imports = [../hyprlock.nix];
-  config = lib.mkIf config.host.laptop {
-    services.hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          lock_cmd = "pgrep hyprlock || hyprlock";
-          before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on";
-        };
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pgrep hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
 
-        listener = [
-          {
-            timeout = 600;
-            on-timeout = "loginctl lock-session";
-          }
-          {
-            timeout = 900;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
+      listener = [
+        {
+          timeout = 600;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 900;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        (lib.mkIf config.host.laptop
           {
             timeout = 1800;
             on-timeout = "systemctl suspend";
-          }
-        ];
-      };
+          })
+      ];
     };
   };
 }
