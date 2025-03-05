ca1ce14f5bc19a9f37bd621a31d05438f4be7008
Yiheng He <yiheng.he@proton.me>
2025-03-05 15:43:48 +0800

    fcitx5: enable pinyin input

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  imports = [./icons.nix ./stylix.nix ./wluma.nix ./copyq.nix ./imv.nix ../font];
+  imports = [./icons.nix ./stylix.nix ./wluma.nix ./copyq.nix ./imv.nix ../font ./fcitx5.nix];
   home.packages = with pkgs; [
     desktop-file-utils
   ];
---
+++ home/desktop/fcitx5.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}: {
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [fcitx5-chinese-addons];
+  };
+}
