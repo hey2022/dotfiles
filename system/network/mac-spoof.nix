3f3f9fa0b113a982755abec8640a88a50f3fb43f
Yiheng He <yiheng.he@proton.me>
2025-03-17 16:39:31 +0800

    network: spoof ethernet mac address

--- system/network/mac-spoof.nix
+++ system/network/mac-spoof.nix
@@ -1,3 +1,8 @@
-{...}: {
-  networking.networkmanager.wifi.macAddress = "B4:AD:A3:F8:84:05";
+{...}: let
+  spoofed-mac-address = "B4:AD:A3:F8:84:05";
+in {
+  networking.networkmanager = {
+    wifi.macAddress = spoofed-mac-address;
+    ethernet.macAddress = spoofed-mac-address;
+  };
 }
