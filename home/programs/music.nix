f5d89f807ce4dac3ed472ff79d4ff9a5d29c1471
Yiheng He <yiheng.he@proton.me>
2025-03-17 18:57:20 +0800

    flake: update inputs

--- home/programs/music.nix
+++ home/programs/music.nix
@@ -1,14 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    (supersonic-wayland.overrideAttrs (old: {
-      version = "git";
-      src = fetchFromGitHub {
-        owner = "dweymouth";
-        repo = "supersonic";
-        rev = "adf6d7c324b87e863ff8ae684ac1c6cfcb6227c6";
-        hash = "sha256-ua2INyKPncXDOwzmKrgnRCb7q8CFEApEaYuBbQeau98=";
-      };
-      vendorHash = "sha256-5LxYD9kLUvKgXmDCw1SNBM6ay8Vayj+PyoZRVptSM0c=";
-    }))
+    supersonic-wayland
   ];
 }
