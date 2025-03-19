4fe6c0c128ad02742a43112ac568b638b46778cb
Yiheng He <yiheng.he@proton.me>
2025-03-19 16:45:57 +0800

    foot: set font to IosevkaCustom

--- home/desktop/terminal/foot.nix
+++ home/desktop/terminal/foot.nix
@@ -7,5 +7,10 @@
     enable = true;
     package = config.lib.nixGL.wrap pkgs.foot;
     server.enable = true;
+    settings = {
+      main = {
+        font = "IosevkaCustom:size=12";
+      };
+    };
   };
 }
