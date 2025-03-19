a7f5727ac59af0749619b2d3554daa6bb1d2ee7b
Yiheng He <yiheng.he@proton.me>
2025-03-19 16:40:14 +0800

    hyprlock: catppuccin rice

--- home/desktop/hyprlock.nix
+++ home/desktop/hyprlock.nix
@@ -10,29 +10,84 @@
       if config.home.isNixOS
       then pkgs.hyprlock
       else pkgs.emptyDirectory;
-    settings = {
-      general = {
-        grace = 3;
-        hide_cursor = true;
-      };
-      label = {
-        text = "$TIME";
-        color = "rgb(${config.lib.stylix.colors.base05})";
-        shadow_color = "rgb(${config.lib.stylix.colors.base00})";
-        shadow_passes = 1;
-        shadow_size = 1;
+    extraConfig = ''
+      $accent = $mauve
+      $accentAlpha = $mauveAlpha
+      $font = Inter
 
-        position = "0, 100";
-        font_family = "Inter";
-        font_size = 64;
+      # GENERAL
+      general {
+        disable_loading_bar = true
+        hide_cursor = true
+      }
+
+      # BACKGROUND
+      background {
+        monitor =
+        path = screenshot
+        blur_passes = 2
+        color = $base
+      }
+
+      # TIME
+      label {
+        monitor =
+        text = $TIME
+        color = $text
+        font_size = 90
+        font_family = $font
+        position = -30, 0
+        halign = right
+        valign = top
+      }
+
+      # DATE
+      label {
+        monitor =
+        text = cmd[update:43200000] date +"%A, %d %B %Y"
+        color = $text
+        font_size = 25
+        font_family = $font
+        position = -30, -150
+        halign = right
+        valign = top
+      }
+
+      # FINGERPRINT
+      {
+        monitor = "";
+        text = "$FPRINTPROMPT";
+        color = "$text";
+        font_size = 14;
+        font_family = font;
+        position = "0, -107";
         halign = "center";
         valign = "center";
-      };
-      background = {
-        path = lib.mkForce "screenshot";
-        blur_passes = 2;
-      };
-    };
+      }
+
+      # INPUT FIELD
+      input-field {
+        monitor =
+        size = 300, 60
+        outline_thickness = 4
+        dots_size = 0.2
+        dots_spacing = 0.2
+        dots_center = true
+        outer_color = $accent
+        inner_color = $surface0
+        font_color = $text
+        fade_on_empty = false
+        placeholder_text = <span foreground="##$textAlpha"><i>󰌾 Logged in as </i><span foreground="##$accentAlpha">$USER</span></span>
+        hide_input = false
+        check_color = $accent
+        fail_color = $red
+        fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+        capslock_color = $yellow
+        position = 0, -47
+        halign = center
+        valign = center
+      }
+    '';
   };
   wayland.windowManager.sway.extraConfig = lib.mkIf config.wayland.windowManager.sway.enable (lib.mkAfter "bindsym $mod+shift+escape exec hyprlock");
 }
