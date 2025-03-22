5da442970c8f46c5f2d25aedc70146a02e9fd36f
Yiheng He <yiheng.he@proton.me>
2025-03-22 19:13:48 +0800

    minecraft: open port for voice chat

--- system/services/minecraft.nix
+++ system/services/minecraft.nix
@@ -1,6 +1,11 @@
 {...}: {
   networking.firewall = {
     allowedTCPPorts = [25565];
-    allowedUDPPorts = [25565];
+    allowedUDPPorts = [
+      25565
+
+      # voice chat
+      24454
+    ];
   };
 }
