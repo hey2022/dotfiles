68aab5312ff7ce82d4db807d97f68453c3b5fc0f
Yiheng He <yiheng.he@proton.me>
2025-03-23 20:37:30 +0800

    swaync: set fit-to-screen to false

--- home/desktop/swaync.nix
+++ home/desktop/swaync.nix
@@ -1,15 +1,11 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   services.swaync = {
     enable = true;
     settings = {
       "control-center-margin-top" = 5;
       "control-center-margin-bottom" = 5;
       "control-center-margin-right" = 5;
+      "fit-to-screen" = false;
       "widgets" = [
         "title"
         "dnd"
