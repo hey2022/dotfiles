6c973202ac9fc62463f003427cf23f7b23f56ec6
Yiheng He <yiheng.he@proton.me>
2025-03-24 02:48:34 +0800

    hyprland/rules: resize copyq

--- home/desktop/hyprland/rules.nix
+++ home/desktop/hyprland/rules.nix
@@ -7,7 +7,10 @@
       # Fix some dragging issues with XWayland
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
 
+      # copyq
       "float, class:copyq"
+      "size 25% 25%, class:copyq"
+      "move 100%-w-10, class:copyq"
     ];
   };
 }
