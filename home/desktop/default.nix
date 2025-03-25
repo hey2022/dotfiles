ae3d58011113d609f5dd666e2ce8bb7d48cc63b8
Yiheng He <yiheng.he@proton.me>
2025-03-26 00:07:03 +0800

    gtk: enable

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,13 +1,14 @@
 {pkgs, ...}: {
   imports = [
-    ./icons.nix
-    ./stylix.nix
-    ./catppuccin.nix
-    ./wluma.nix
-    ./copyq.nix
-    ./imv.nix
     ../font
+    ./catppuccin.nix
+    ./copyq.nix
     ./fcitx5.nix
+    ./gtk.nix
+    ./icons.nix
+    ./imv.nix
+    ./stylix.nix
+    ./wluma.nix
   ];
   home.packages = with pkgs; [
     desktop-file-utils
