e4e11e953b68b16ace368b32a7caf7e2cb7ca177
Yiheng He <yiheng.he@proton.me>
2025-03-26 21:44:38 +0800

    yazi/opener: orphan opened program

--- home/programs/cli/yazi.nix
+++ home/programs/cli/yazi.nix
@@ -2,5 +2,17 @@
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
+    settings = {
+      opener = {
+        open = [
+          {
+            run = "xdg-open \"$@\"";
+            orphan = true;
+            desc = "Open";
+            for = "unix";
+          }
+        ];
+      };
+    };
   };
 }
