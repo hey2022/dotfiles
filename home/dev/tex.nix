3b1afa1f25e4b5100f1cb2368f0cef8723c5c72b
Yiheng He <yiheng.he@proton.me>
2025-03-30 16:33:01 +0800

    tex: add latexmkrc config

--- home/dev/tex.nix
+++ home/dev/tex.nix
@@ -1,3 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [texlive.combined.scheme-full texlab];
+  home.file.".latexmkrc".text = ''
+    $pdf_mode = 1;
+    $pdflatex = 'pdflatex -interaction=nonstopmode -shell-escape';
+  '';
 }
