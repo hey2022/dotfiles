fb5593e8b6a3bad06c1806f4b21025f50f3f267d
Yiheng He <yiheng.he@proton.me>
2025-03-31 14:01:48 +0800

    hyprland: slide workspaces

--- home/desktop/hyprland/settings.nix
+++ home/desktop/hyprland/settings.nix
@@ -58,9 +58,12 @@
         "layersOut, 1, 1.5, linear, fade"
         "fadeLayersIn, 1, 1.79, almostLinear"
         "fadeLayersOut, 1, 1.39, almostLinear"
-        "workspaces, 1, 1.94, almostLinear, fade"
-        "workspacesIn, 1, 1.21, almostLinear, fade"
-        "workspacesOut, 1, 1.94, almostLinear, fade"
+        "workspaces, 1, 1.94, almostLinear, slide"
+        "workspacesIn, 1, 1.21, almostLinear, slide"
+        "workspacesOut, 1, 1.94, almostLinear, slide"
+        "specialWorkspace, 1, 1.94, almostLinear, slidefadevert -50%"
+        "specialWorkspaceIn, 1, 1.21, almostLinear, slidefadevert -50%"
+        "specialWorkspaceOut, 1, 1.94, almostLinear, slidefadevert -50%"
       ];
     };
 
