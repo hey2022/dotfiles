ec4f4cac82ba5d2f672816b4e25f3c0d6a4b08cf
Yiheng He <yiheng.he@proton.me>
2025-04-01 15:54:10 +0800

    ghostty: init

---
+++ home/desktop/terminal/ghostty.nix
@@ -0,0 +1,30 @@
+{...}: {
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      gtk-single-instance = true;
+      gtk-tabs-location = "bottom";
+      gtk-wide-tabs = false;
+      background-opacity = 0.9;
+      font-family = [
+        "Iosevka Custom"
+        "Symbols Nerd Font"
+      ];
+      font-size = 12;
+      keybind = [
+        "alt+tab=toggle_tab_overview"
+        "alt+q=close_surface"
+        "alt+h=previous_tab"
+        "alt+l=next_tab"
+        "alt+n=new_tab"
+        "alt+w>v=new_split:right"
+        "alt+w>s=new_split:down"
+        "alt+w>q=close_surface"
+        "alt+w>h=goto_split:left"
+        "alt+w>j=goto_split:bottom"
+        "alt+w>k=goto_split:top"
+        "alt+w>l=goto_split:right"
+      ];
+    };
+  };
+}
