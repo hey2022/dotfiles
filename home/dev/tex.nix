dff7af32f87460eeb85b6a940c9e4653375fa5a6
Yiheng He <yiheng.he@proton.me>
2025-04-01 16:20:13 +0800

    latexmk: use set_tex_cmds

--- home/dev/tex.nix
+++ home/dev/tex.nix
@@ -2,6 +2,6 @@
   home.packages = with pkgs; [texlive.combined.scheme-full texlab];
   home.file.".latexmkrc".text = ''
     $pdf_mode = 4;
-    $lualatex = 'lualatex -interaction=nonstopmode -shell-escape -synctex=1';
+    set_tex_cmds("-interaction=nonstopmode -shell-escape -synctex=1 %O %S");
   '';
 }
