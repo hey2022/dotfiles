02236dd2fe8402832e0c4566cf07df22a097b5e8
Yiheng He <yiheng.he@proton.me>
2025-04-01 16:13:53 +0800

    latexmk: use lualatex

--- home/dev/tex.nix
+++ home/dev/tex.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [texlive.combined.scheme-full texlab];
   home.file.".latexmkrc".text = ''
-    $pdf_mode = 1;
-    $pdflatex = 'pdflatex -interaction=nonstopmode -shell-escape';
+    $pdf_mode = 4;
+    $lualatex = 'lualatex -interaction=nonstopmode -shell-escape';
   '';
 }
