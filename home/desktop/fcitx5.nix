7e7ceabc3e2ab0ede9145222b7bddac9893c242f
Yiheng He <yiheng.he@proton.me>
2025-04-01 13:08:42 +0800

    fcitx5: set waylandFrontend to true

--- home/desktop/fcitx5.nix
+++ home/desktop/fcitx5.nix
@@ -1,7 +1,10 @@
 {pkgs, ...}: {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [fcitx5-chinese-addons];
+    fcitx5 = {
+      addons = with pkgs; [fcitx5-chinese-addons];
+      waylandFrontend = true;
+    };
   };
   home.sessionVariables = {
     SDL_IM_MODULE = "fcitx"; # IME support in wechat
