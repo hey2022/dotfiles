ab2bdd135bf2165456926a2143082240bfcd8233
Yiheng He <yiheng.he@proton.me>
2025-04-03 11:33:58 +0800

    mimeApps: set imv as default image viewer

--- home/xdg/mimeApps.nix
+++ home/xdg/mimeApps.nix
@@ -3,6 +3,10 @@
     enable = true;
     defaultApplications = {
       "application/pdf" = ["org.pwmt.zathura.desktop"];
+      "application/pdf" = "org.pwmt.zathura.desktop";
+      "image/jpeg" = "imv.desktop";
+      "image/png" = "imv.desktop";
+      "image/webp" = "imv.desktop";
     };
   };
 }
