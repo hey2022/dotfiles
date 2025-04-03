bfb39858cc157814175e4bb94ce20ba070de4d62
Yiheng He <yiheng.he@proton.me>
2025-04-03 21:08:18 +0800

    Revert "kanata: increase hold timeout to 500ms"
    This reverts commit efa7eba8bb520b39857ef58ae8589612cf19f27f.

--- system/services/kanata.nix
+++ system/services/kanata.nix
@@ -6,7 +6,7 @@
         (defsrc)
 
         (deflayermap (base-layer)
-          caps (tap-hold-press 200 500 caps lctl))
+          caps (tap-hold-press 200 200 caps lctl))
       '';
       extraDefCfg = ''
         process-unmapped-keys yes
