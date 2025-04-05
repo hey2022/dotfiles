904ab43e69ac4848c7a457fa549e78c991770ac6
Yiheng He <yiheng.he@proton.me>
2025-04-06 04:47:31 +0800

    catppuccin: update home manager module name

--- home/desktop/catppuccin.nix
+++ home/desktop/catppuccin.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  imports = [inputs.catppuccin.homeManagerModules.catppuccin];
+  imports = [inputs.catppuccin.homeModules.catppuccin];
   catppuccin = {
     enable = true;
     flavor = "mocha";
