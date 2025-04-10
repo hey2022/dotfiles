7842f37761328ced132f535ec4e49c54479dca05
Yiheng He <yiheng.he@proton.me>
2025-04-10 13:53:53 +0800

    hyprland: improve animations

--- home/desktop/hyprland/settings.nix
+++ home/desktop/hyprland/settings.nix
@@ -37,33 +37,21 @@
     animations = {
       enabled = true;
       bezier = [
-        "easeOutQuint,0.23,1,0.32,1"
-        "easeInOutCubic,0.65,0.05,0.36,1"
-        "linear,0,0,1,1"
-        "almostLinear,0.5,0.5,0.75,1.0"
-        "quick,0.15,0,0.1,1"
+        "overshot,0.05,0.9,0.1,1.05"
+        "exit,0.9,-0.2,0.95,0.05"
       ];
 
       animation = [
-        "global, 1, 10, default"
-        "border, 1, 5.39, easeOutQuint"
-        "windows, 1, 4.79, easeOutQuint"
-        "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-        "windowsOut, 1, 1.49, linear, popin 87%"
-        "fadeIn, 1, 1.73, almostLinear"
-        "fadeOut, 1, 1.46, almostLinear"
-        "fade, 1, 3.03, quick"
-        "layers, 1, 3.81, easeOutQuint"
-        "layersIn, 1, 4, easeOutQuint, fade"
-        "layersOut, 1, 1.5, linear, fade"
-        "fadeLayersIn, 1, 1.79, almostLinear"
-        "fadeLayersOut, 1, 1.39, almostLinear"
-        "workspaces, 1, 1.94, almostLinear, slide"
-        "workspacesIn, 1, 1.21, almostLinear, slide"
-        "workspacesOut, 1, 1.94, almostLinear, slide"
-        "specialWorkspace, 1, 1.94, almostLinear, slidefadevert -50%"
-        "specialWorkspaceIn, 1, 1.21, almostLinear, slidefadevert -50%"
-        "specialWorkspaceOut, 1, 1.94, almostLinear, slidefadevert -50%"
+        "windows, 1, 3, overshot, popin 70%"
+        "windowsOut, 1, 3, exit, slide"
+        "borderangle, 1, 7, default"
+        "fade, 1, 8, default"
+        "fadeSwitch, 1, 15, default"
+        "fadeShadow, 1, 15, default"
+        "fadeDim, 1, 15, default"
+        "border, 1, 15, default"
+        "workspaces, 1, 3, default, slidefade 100%"
+        "specialWorkspace, 1, 3, default, slidefadevert -50%"
       ];
     };
 
