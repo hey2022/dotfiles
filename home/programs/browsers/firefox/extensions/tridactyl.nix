62d637e40b2fbac7adc5ef2cf8d5712859e9bd1f
Yiheng He <yiheng.he@proton.me>
2025-04-13 01:57:21 +0800

    tridactyl: set editorcmd to emacs

--- home/programs/browsers/firefox/extensions/tridactyl.nix
+++ home/programs/browsers/firefox/extensions/tridactyl.nix
@@ -1,8 +1,12 @@
 {
+  inputs,
   config,
   pkgs,
   ...
 }: {
+  imports = [
+    "${inputs.self}/home/dev/emacs"
+  ];
   programs.firefox.nativeMessagingHosts = with pkgs; [tridactyl-native];
   xdg.configFile."tridactyl/tridactylrc".source = config.lib.hm.mkFlakeSymlink ./tridactylrc;
 }
