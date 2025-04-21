d56df8183ca5d3d68611e9aa6765e8ff982ba155
Yiheng He <yiheng.he@proton.me>
2025-04-21 20:12:08 +0800

    flatpak: add sjtug gpg key

--- home/services/flatpak.nix
+++ home/services/flatpak.nix
@@ -8,6 +8,10 @@
       {
         name = "flathub";
         location = "https://mirrors.sjtug.sjtu.edu.cn/flathub";
+        gpg-import = builtins.fetchurl {
+          url = "https://mirror.sjtu.edu.cn/flathub/flathub.gpg";
+          sha256 = "1amf8767ipfvyb31k7lr2s3i793s1vz5psqb8sb0g771qjmj1p4b";
+        };
       }
     ];
   };
