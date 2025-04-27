01324766a5d741c452ebbe1ea29afb44de9c7ad8
Yiheng He <yiheng.he@proton.me>
2025-04-27 20:27:56 +0800

    sober: add home.activation for sober config

--- home/programs/games/roblox/sober.nix
+++ home/programs/games/roblox/sober.nix
@@ -1,3 +1,10 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.flatpak.packages = ["org.vinegarhq.Sober"];
+  home.activation.sober-config = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    install -D ${config.home.dotfiles}/home/programs/games/roblox/config.json ${config.home.homeDirectory}/.var/app/org.vinegarhq.Sober/config/sober/config.json
+  '';
 }
