4b4dffeb689ae27b7c737acd84a9d55ec14827c8
Yiheng He <yiheng.he@proton.me>
2025-05-04 00:24:52 +0800

    prometheus/node_exporter: fix listenAddress

--- system/services/prometheus/node_exporter.nix
+++ system/services/prometheus/node_exporter.nix
@@ -1,8 +1,14 @@
-{config, ...}: {
+{config, ...}: let
+  cfg = config.services.prometheus.exporters.node;
+in {
   services.prometheus.exporters.node = {
     enable = true;
     openFirewall = true;
-    listenAddress = config.networking.hostName;
+    listenAddress = "0.0.0.0";
     enabledCollectors = ["systemd"];
+
+    firewallRules = ''
+      iifname "tailscale0" tcp dport ${toString cfg.port} counter accept
+    '';
   };
 }
