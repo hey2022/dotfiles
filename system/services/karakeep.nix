299258022d1f9b2184d1d7571233be121175b7d1
Yiheng He <yiheng.he@proton.me>
2025-05-04 15:39:25 +0800

    karakeep: allow through firewall

--- system/services/karakeep.nix
+++ system/services/karakeep.nix
@@ -1,8 +1,15 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.karakeep = {
     enable = true;
     extraEnvironment = {
       PORT = "9000";
     };
+  };
+  networking.firewall = {
+    allowedTCPPorts = [(lib.strings.toInt config.services.karakeep.extraEnvironment.PORT)];
   };
 }
