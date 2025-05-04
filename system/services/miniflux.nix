2a4fe827adf3617c709a2336e08478fea6565d2a
Yiheng He <yiheng.he@proton.me>
2025-05-04 15:38:12 +0800

    miniflux: allow through firewall

--- system/services/miniflux.nix
+++ system/services/miniflux.nix
@@ -1,9 +1,14 @@
-{...}: {
+{...}: let
+  port = 8081;
+in {
   services.miniflux = {
     enable = true;
     adminCredentialsFile = /etc/nixos/miniflux-admin-credentials;
     config = {
-      LISTEN_ADDR = "0.0.0.0:8081";
+      LISTEN_ADDR = "0.0.0.0:${toString port}";
     };
+  };
+  networking.firewall = {
+    allowedTCPPorts = [port];
   };
 }
