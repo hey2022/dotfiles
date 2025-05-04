f84e537ac0793021a12e4b4aac0250aee37e2317
Yiheng He <yiheng.he@proton.me>
2025-05-04 15:30:51 +0800

    miniflux: listen on all

--- system/services/miniflux.nix
+++ system/services/miniflux.nix
@@ -3,7 +3,7 @@
     enable = true;
     adminCredentialsFile = /etc/nixos/miniflux-admin-credentials;
     config = {
-      LISTEN_ADDR = "localhost:8081";
+      LISTEN_ADDR = "0.0.0.0:8081";
     };
   };
 }
