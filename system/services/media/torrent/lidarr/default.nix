4075571cfcf55806749c7fb0c42ea09af4d2155b
Yiheng He <yiheng.he@proton.me>
2025-05-09 15:20:44 +0800

    lidarr: setup caddy

--- system/services/media/torrent/lidarr/default.nix
+++ system/services/media/torrent/lidarr/default.nix
@@ -1,8 +1,10 @@
-{...}: {
+{config, ...}: {
   imports = [./beets.nix];
   services.lidarr = {
     enable = true;
     group = "media";
-    openFirewall = true;
   };
+  services.caddy.virtualHosts."lidarr.${config.host.address}".extraConfig = ''
+    reverse_proxy localhost:8686
+  '';
 }
