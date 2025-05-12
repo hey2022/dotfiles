903ee65f09cc602fe022f56bc98434ce18c7419d
Yiheng He <yiheng.he@proton.me>
2025-05-12 19:32:00 +0800

    boot: switch to linux zen kernel
    Switch to linux-tkg once its packaged

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -1,9 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    # https://github.com/NixOS/nixpkgs/issues/287189
+    kernelPackages = pkgs.linuxPackages_zen;
     kernel.sysctl."kernel.sysrq" = 1;
   };
 }
