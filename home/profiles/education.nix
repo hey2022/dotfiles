18c7e4bab7b920ecf0dda764c858aae2e3780725
Yiheng He <yiheng.he@proton.me>
2025-05-13 13:28:05 +0800

    education: add TODO refactor

--- home/profiles/education.nix
+++ home/profiles/education.nix
@@ -3,6 +3,7 @@
   pkgs,
   ...
 }: {
+  # TODO: refactor this into a proper profile
   imports = [../programs/email.nix ../programs/productivity ../programs/dictionary.nix ../programs/anki.nix ../programs/calculator.nix ../shell/scripts/ap-dl];
   home.packages = with pkgs; [
     zotero
