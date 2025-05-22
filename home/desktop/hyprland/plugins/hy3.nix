296aeb697f055ee183c92fafbd27f14824c715c7
Yiheng He <yiheng.he@proton.me>
2025-05-22 11:00:35 +0800

    hy3: use unstable nixpkgs

--- home/desktop/hyprland/plugins/hy3.nix
+++ home/desktop/hyprland/plugins/hy3.nix
@@ -2,12 +2,10 @@
   config,
   lib,
   pkgs,
-  pkgs-git,
   ...
 }: {
   wayland.windowManager.hyprland = lib.mkIf config.wayland.windowManager.hyprland.hy3 {
-    # HACK: https://nixpk.gs/pr-tracker.html?pr=408284
-    plugins = [pkgs-git.hyprlandPlugins.hy3];
+    plugins = [pkgs.hyprlandPlugins.hy3];
     settings = {
       general = {
         layout = lib.mkForce "hy3";
