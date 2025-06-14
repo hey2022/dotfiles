e747de16e92a72fd157ee295e3e200b8dc0d180f
Yiheng He <yiheng.he@proton.me>
2025-06-14 09:12:35 +0800

    dev/python: add scikit-learn

--- home/dev/python.nix
+++ home/dev/python.nix
@@ -1,6 +1,14 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  datascience = with pkgs.python3Packages; [
+    matplotlib
+    numpy
+    pandas
+    scikit-learn
+    scipy
+  ];
+in {
   home.packages = with pkgs; [
-    (python3.withPackages (python: with python; [numpy pandas matplotlib scipy]))
+    (python3.withPackages (python: datascience))
     basedpyright
     uv
     ruff
