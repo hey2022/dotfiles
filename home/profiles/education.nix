907e5a0bb9d111259e60d747bf40d4954bf3b197
Yiheng He <yiheng.he@proton.me>
2025-06-17 11:06:19 +0800

    profiles/education: add teams-for-linux

--- home/profiles/education.nix
+++ home/profiles/education.nix
@@ -4,9 +4,17 @@
   ...
 }: {
   # TODO: refactor this into a proper profile
-  imports = [../programs/email.nix ../programs/productivity ../programs/dictionary.nix ../programs/anki.nix ../programs/calculator.nix ../shell/scripts/ap-dl];
+  imports = [
+    ../programs/email.nix
+    ../programs/productivity
+    ../programs/dictionary.nix
+    ../programs/anki.nix
+    ../programs/calculator.nix
+    ../shell/scripts/ap-dl
+  ];
   home.packages = with pkgs; [
-    zotero
     inputs.tls-xb.packages.${pkgs.system}.default
+    teams-for-linux
+    zotero
   ];
 }
