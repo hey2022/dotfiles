423d8a92ad5d9fbe5931c631ea13783e9249534c
Yiheng He <yiheng.he@proton.me>
2025-06-21 19:24:44 +0800

    services/caddy: update plugin hash

--- system/services/caddy.nix
+++ system/services/caddy.nix
@@ -8,7 +8,7 @@
     environmentFile = config.sops.secrets.caddy.path;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/duckdns@v0.5.0"];
-      hash = "sha256-ZdvxtHiJAyKIOVUAFns7WsfQddfO/fD1euyGFaBp9W8=";
+      hash = "sha256-83ETc9K4T13Ws8gVOYwLarhuCA48Drs/i3rVLBMHyrc=";
     };
   };
   services.tailscale.permitCertUid = config.services.caddy.user;
