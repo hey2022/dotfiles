431e6419ab998a19fea2382abdb49bfe472e0906
Yiheng He <yiheng.he@proton.me>
2025-06-23 03:16:44 +0800

    programs/clash-verge: use rules.yaml

--- home/programs/proxy/clash-verge/default.nix
+++ home/programs/proxy/clash-verge/default.nix
@@ -1,10 +1,5 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     clash-verge-rev
   ];
-  home.file.".local/share/io.github.clash-verge-rev.clash-verge-rev/profiles/Merge.yaml".source = config.lib.hm.mkFlakeSymlink ./Merge.yaml;
 }
