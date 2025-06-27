3fb322f013858ebd87d5035e371e3e96102537f9
Yiheng He <yiheng.he@proton.me>
2025-06-27 15:44:47 +0800

    clash-verge: manually roll out clash-verge.service

--- system/network/proxy/clash-verge.nix
+++ system/network/proxy/clash-verge.nix
@@ -1,7 +1,17 @@
-{...}: {
+{config, ...}: {
   programs.clash-verge = {
     enable = true;
-    serviceMode = true;
+    serviceMode = false;
     tunMode = true;
+  };
+  # HACK: https://github.com/NixOS/nixpkgs/issues/420460
+  systemd.services.clash-verge = {
+    enable = true;
+    description = "Clash Verge Service Mode";
+    serviceConfig = {
+      ExecStart = "${config.programs.clash-verge.package}/bin/clash-verge-service";
+      Restart = "on-failure";
+    };
+    wantedBy = ["multi-user.target"];
   };
 }
