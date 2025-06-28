7907ed0ba58ad8fee6d616ab08230f635fb288f0
Yiheng He <yiheng.he@proton.me>
2025-06-29 03:40:17 +0800

    hyprland/pyprland: add magnify plugin

--- home/desktop/hyprland/plugins/pyprland/default.nix
+++ home/desktop/hyprland/plugins/pyprland/default.nix
@@ -13,6 +13,8 @@
       bind = [
         "$mod, B, exec, pypr fetch_client_menu"
         "$mod, F, exec, pypr layout_center toggle"
+        "$mod, Z, exec, pypr zoom ++0.5"
+        "$mod SHIFT, Z, exec, pypr zoom"
       ];
     };
     xdg.configFile."hypr/pyprland.toml".source = config.lib.hm.mkFlakeSymlink ./pyprland.toml;
