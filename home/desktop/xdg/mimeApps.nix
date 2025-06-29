9e76d3dd6936f2b7d675956a0e2b397abbaab8ea
Yiheng He <yiheng.he@proton.me>
2025-06-29 23:54:44 +0800

    xdg/mimeApps: add firefox secondary browser

--- home/desktop/xdg/mimeApps.nix
+++ home/desktop/xdg/mimeApps.nix
@@ -8,9 +8,9 @@
       "image/webp" = "imv.desktop";
       "image/svg+xml" = "org.inkscape.Inkscape.desktop";
       "text/plain" = "emacsclient.desktop";
-      "text/html" = "zen-beta.desktop";
-      "x-scheme-handler/http" = "zen-beta.desktop";
-      "x-scheme-handler/https" = "zen-beta.desktop";
+      "text/html" = ["zen-beta.desktop" "firefox.desktop"];
+      "x-scheme-handler/http" = ["zen-beta.desktop" "firefox.desktop"];
+      "x-scheme-handler/https" = ["zen-beta.desktop" "firefox.desktop"];
     };
   };
 }
