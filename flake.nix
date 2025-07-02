5ab9814ce4ea70286f77aa28c25e0dd3ab316b74
Yiheng He <yiheng.he@proton.me>
2025-07-03 00:57:49 +0800

    flake: zen-browser follow home-manager

--- flake.nix
+++ flake.nix
@@ -70,6 +70,7 @@
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
     zen-browser-catppuccin = {
       url = "github:catppuccin/zen-browser";
