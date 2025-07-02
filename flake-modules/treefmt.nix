1cbf2233a72e312900ae55c4cf045238c6a25c5f
Yiheng He <yiheng.he@proton.me>
2025-07-02 23:07:00 +0800

    treefmt: remove unnecessary function

--- flake-modules/treefmt.nix
+++ flake-modules/treefmt.nix
@@ -1,17 +1,15 @@
 { inputs, ... }:
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
-  perSystem =
-    { ... }:
-    {
-      treefmt = {
-        projectRootFile = "flake.nix";
-        programs = {
-          nixfmt.enable = true;
-          prettier.enable = true;
-          shellcheck.enable = true;
-          taplo.enable = true;
-        };
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs = {
+        nixfmt.enable = true;
+        prettier.enable = true;
+        shellcheck.enable = true;
+        taplo.enable = true;
       };
     };
+  };
 }
