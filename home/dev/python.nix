7f8c3b854d0c66dcd2678557705cb81a9b1a4de0
Yiheng He <yiheng.he@proton.me>
2025-07-06 02:42:39 +0800

    python: remove libraries

--- home/dev/python.nix
+++ home/dev/python.nix
@@ -1,18 +1,7 @@
 { pkgs, ... }:
-let
-  datascience = with pkgs.python3Packages; [
-    jupyterlab
-    matplotlib
-    numpy
-    pandas
-    scikit-learn
-    scipy
-    seaborn
-  ];
-in
 {
   home.packages = with pkgs; [
-    (python3.withPackages (python: datascience))
+    python3
     basedpyright
     uv
     ruff
