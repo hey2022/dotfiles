566764919c9720fcf319152ae6224f5acacc6dda
Yiheng He <yiheng.he@proton.me>
2025-07-09 04:46:45 +0800

    gfm: use mktemp for prefix and suffix file

--- home/shell/scripts/gfm/gfm.sh
+++ home/shell/scripts/gfm/gfm.sh
@@ -6,15 +6,17 @@ if [[ -z "$1" ]]; then
 fi
 
 cleanup() {
-    rm "$tmpfile" /tmp/gfm-prefix.html /tmp/gfm-suffix.html
+    rm "$tmp_output" "$tmp_prefix" "$tmp_suffix"
     exit 0
 }
 trap cleanup INT
 
-tmpfile=$(mktemp /tmp/gfm-XXXXXX.html)
-echo '<div class="markdown-body">' >/tmp/gfm-prefix.html
-echo '</div>' >/tmp/gfm-suffix.html
+tmp_output=$(mktemp /tmp/gfm-XXXXXX.html)
+tmp_prefix=$(mktemp /tmp/gfm-prefix-XXXXXX.html)
+tmp_suffix=$(mktemp /tmp/gfm-suffix-XXXXXX.html)
+echo '<div class="markdown-body">' >"$tmp_prefix"
+echo '</div>' >"$tmp_suffix"
 
-live-server "$tmpfile" &
-cmd="comrak --gfm $1 | pandoc -s --metadata pagetitle=\"GFM Preview\" -c 'https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css' --include-before-body=/tmp/gfm-prefix.html --include-after-body=/tmp/gfm-suffix.html -o $tmpfile"
+live-server "$tmp_output" &
+cmd="comrak --gfm $1 | pandoc -s --metadata pagetitle=\"GFM Preview\" -c 'https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css' --include-before-body=$tmp_prefix --include-after-body=$tmp_suffix -o $tmp_output"
 echo "$1" | entr -s "$cmd"
