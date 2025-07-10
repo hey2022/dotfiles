a7dc097367803dff29a4366ddefacbae9ee02842
Yiheng He <yiheng.he@proton.me>
2025-07-10 10:10:42 +0800

    home/nix: add nix-access-token

--- home/nix.nix
+++ home/nix.nix
@@ -6,6 +6,10 @@
 }:
 
 {
+  sops.secrets.nix-access-tokens = {
+    sopsFile = "${inputs.self}/secrets/nix-access-tokens";
+    format = "binary";
+  };
   _module.args.pkgs-stable = import inputs.nixpkgs-stable {
     inherit (pkgs.stdenv.hostPlatform) system;
     inherit (config.nixpkgs) config;
@@ -17,5 +21,8 @@
       "nix-command"
       "flakes"
     ];
+    extraOptions = ''
+      !include ${config.sops.secrets.nix-access-tokens.path}
+    '';
   };
 }
