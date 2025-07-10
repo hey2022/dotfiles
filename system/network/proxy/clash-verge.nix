f29f41b2190194671c2e713e517bc43d5bb59c59
Yiheng He <yiheng.he@proton.me>
2025-07-10 10:11:40 +0800

    clash-verge: remove service workaround

--- system/network/proxy/clash-verge.nix
+++ system/network/proxy/clash-verge.nix
@@ -1,18 +1,8 @@
-{ config, ... }:
+{ ... }:
 {
   programs.clash-verge = {
     enable = true;
-    serviceMode = false;
+    serviceMode = true;
     tunMode = true;
-  };
-  # HACK: https://nixpk.gs/pr-tracker.html?pr=420530
-  systemd.services.clash-verge = {
-    enable = true;
-    description = "Clash Verge Service Mode";
-    serviceConfig = {
-      ExecStart = "${config.programs.clash-verge.package}/bin/clash-verge-service";
-      Restart = "on-failure";
-    };
-    wantedBy = [ "multi-user.target" ];
   };
 }
