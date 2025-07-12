9b6b80fe13597c9a21c6ca0477e5b4975dd66647
Yiheng He <yiheng.he@proton.me>
2025-07-12 20:42:17 +0800

    clash-verge: set proxy in uwsm instead

--- home/programs/proxy/clash-verge/default.nix
+++ home/programs/proxy/clash-verge/default.nix
@@ -3,4 +3,9 @@
   home.packages = with pkgs; [
     clash-verge-rev
   ];
+  xdg.configFile."uwsm/env".text = ''
+    export https_proxy=http://127.0.0.1:7897
+    export http_proxy=http://127.0.0.1:7897
+  '';
+
 }
--- system/network/proxy/clash-verge.nix
+++ system/network/proxy/clash-verge.nix
@@ -4,8 +4,4 @@
     serviceMode = true;
     tunMode = true;
   };
-  environment.sessionVariables = {
-    https_proxy = "http://127.0.0.1:7897";
-    http_proxy = "http://127.0.0.1:7897";
-  };
 }
