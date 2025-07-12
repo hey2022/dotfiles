acba1c1566989a3b69de0111c251e3956780cb0d
Yiheng He <yiheng.he@proton.me>
2025-07-12 18:40:31 +0800

    hyprland: add uwsm option

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -1,9 +1,13 @@
 {
   inputs,
+  config,
   lib,
   pkgs,
   ...
 }:
+let
+  cfg = config.wayland.windowManager.hyprland;
+in
 {
   imports = [
     ../../dev/jq.nix
@@ -17,19 +21,24 @@
     ./plugins
     ./rules.nix
     ./settings.nix
+    ./startup.nix
   ];
   options = {
-    wayland.windowManager.hyprland.hy3 = lib.mkEnableOption "hy3";
+    wayland.windowManager.hyprland = {
+      hy3 = lib.mkEnableOption "hy3";
+      uwsm = lib.mkEnableOption "uwsm";
+    };
   };
   config = {
     wayland.windowManager.hyprland = {
       enable = true;
       systemd.enable = false;
       hy3 = true;
+      uwsm = true;
     };
     home.packages = [
       inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
-    ];
+    ] ++ lib.optionals cfg.uwsm [ pkgs.uwsm ];
     home.sessionVariables.NIXOS_OZONE_WL = "1";
     programs = {
       rofi.enable = true;
