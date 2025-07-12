fddf98e80a06f07d53255b6b4f5ee9a5bba1bac9
Yiheng He <yiheng.he@proton.me>
2025-07-12 14:42:14 +0800

    shell: add -p flag to bat alias

--- home/shell/aliases.nix
+++ home/shell/aliases.nix
@@ -1,11 +1,11 @@
-_: {
+{
   home.shellAliases = {
     eza = "eza --icons";
     ls = "eza";
     la = "eza -lag";
     ll = "eza -lg";
     cd = "z";
-    cat = "bat";
+    cat = "bat -p";
     ec = "emacsclient -nw";
     ff = "fastfetch";
     uctl = "systemctl --user";
