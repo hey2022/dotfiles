00c69879a4fbcc6cae8d0e1df05c99ee4ba5b9b6
Yiheng He <yiheng.he@proton.me>
2025-07-12 17:40:56 +0800

    system: use latest linux kernel

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -5,8 +5,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    # TODO: Use switch to tkg https://github.com/NixOS/nixpkgs/issues/287189
-    kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernel.sysctl = {
       "kernel.sysrq" = 1;
       "vm.swappiness" = 10;
