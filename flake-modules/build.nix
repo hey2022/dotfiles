a0d602fd090433d68e761cceb52287f8259082fe
Yiheng He <yiheng.he@proton.me>
2025-07-12 14:29:56 +0800

    flake: fix package outputs

--- flake-modules/build.nix
+++ flake-modules/build.nix
@@ -5,30 +5,45 @@
 }:
 
 {
-  flake.lib.build = system: rec {
-    mkNixpkgs = import self.packages.${system}.nixpkgs-patched {
-      inherit system;
-      config = import ../common/nixpkgs.nix;
-      overlays = [
-        (_final: _prev: builtins.mapAttrs (_: package: package) self.packages.${system})
-      ];
+  flake.lib.build =
+    system:
+    let
+      pkgs = import inputs.nixpkgs { inherit system; };
+      nixpkgs-patched = pkgs.applyPatches {
+        name = "nixpkgs-patched";
+        src = inputs.nixpkgs;
+        patches = [ ];
+      };
+      home-manager-patched = pkgs.applyPatches {
+        name = "home-manager-patched";
+        src = inputs.home-manager;
+        patches = [ ];
+      };
+    in
+    rec {
+      mkNixpkgs = import nixpkgs-patched {
+        inherit system;
+        config = import ../common/nixpkgs.nix;
+        overlays = [
+          (_final: _prev: builtins.mapAttrs (_: package: package) self.packages.${system})
+        ];
+      };
+      mkSystem =
+        modules:
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit system modules;
+          pkgs = mkNixpkgs;
+          specialArgs = { inherit inputs; };
+        };
+      mkHome =
+        modules:
+        let
+          home-manager' = import home-manager-patched { };
+        in
+        home-manager'.lib.homeManagerConfiguration {
+          inherit modules;
+          pkgs = mkNixpkgs;
+          extraSpecialArgs = { inherit inputs; };
+        };
     };
-    mkSystem =
-      modules:
-      inputs.nixpkgs.lib.nixosSystem {
-        inherit system modules;
-        pkgs = mkNixpkgs;
-        specialArgs = { inherit inputs; };
-      };
-    mkHome =
-      modules:
-      let
-        home-manager' = import self.packages.${system}.home-manager-patched { };
-      in
-      home-manager'.lib.homeManagerConfiguration {
-        inherit modules;
-        pkgs = mkNixpkgs;
-        extraSpecialArgs = { inherit inputs; };
-      };
-  };
 }
--- flake-modules/pkgs.nix
+++ flake-modules/pkgs.nix
@@ -1,19 +1,7 @@
-{ inputs, ... }:
-
 {
   perSystem =
     { pkgs, ... }:
     {
       packages.cliprust = pkgs.callPackage ../pkgs/cliprust { };
-      packages.nixpkgs-patched = pkgs.applyPatches {
-        name = "nixpkgs-patched";
-        src = inputs.nixpkgs;
-        patches = [ ];
-      };
-      packages.home-manager-patched = pkgs.applyPatches {
-        name = "home-manager-patched";
-        src = inputs.home-manager;
-        patches = [ ];
-      };
     };
 }
