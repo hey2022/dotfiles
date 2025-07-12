2e9f998d68835d5cae510c491b596b4062a429ab
Yiheng He <yiheng.he@proton.me>
2025-07-12 20:59:18 +0800

    hyprland: disable focus_on_activate

--- home/desktop/hyprland/settings.nix
+++ home/desktop/hyprland/settings.nix
@@ -64,7 +64,7 @@ _: {
     misc = {
       force_default_wallpaper = 0;
       disable_hyprland_logo = true;
-      focus_on_activate = true;
+      focus_on_activate = false;
     };
 
     binds = {
