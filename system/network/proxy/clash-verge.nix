d87b661abc6baa7d32dcf69478bfe644a00c5d2d
Yiheng He <yiheng.he@proton.me>
2025-07-12 16:48:55 +0800

    clash-verge: set proxy variables

--- system/network/proxy/clash-verge.nix
+++ system/network/proxy/clash-verge.nix
@@ -1,7 +1,11 @@
-_: {
+{
   programs.clash-verge = {
     enable = true;
     serviceMode = true;
     tunMode = true;
+  };
+  environment.sessionVariables = {
+    https_proxy = "http://127.0.0.1:7897";
+    http_proxy = "http://127.0.0.1:7897";
   };
 }
