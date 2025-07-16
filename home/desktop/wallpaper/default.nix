99441acbacc5b90f649827cb1a8824a32a4d460a
Yiheng He <yiheng.he@proton.me>
2025-07-17 06:43:50 +0800

    wallpaper: remove wallpaper updater

--- home/desktop/wallpaper/default.nix
+++ home/desktop/wallpaper/default.nix
@@ -1,32 +1,6 @@
-{ pkgs, lib, ... }:
-let
-  wallpaper-updater = pkgs.writeShellApplication {
-    name = "wallpaper-updater";
-    runtimeInputs = with pkgs; [
-      coreutils
-      git
-    ];
-    text = builtins.readFile ./wallpaper-updater.sh;
-  };
-in
 {
   imports = [
     ./waypaper.nix
   ];
   services.swww.enable = true;
-  systemd.user.services.wallpaper-updater = {
-    Unit = {
-      Description = "Update wallpaper collection";
-      After = "network-online.target";
-      Wants = "network-online.target";
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${lib.getExe wallpaper-updater}";
-    };
-  };
-  home.activation.download-wallpapers = lib.hm.dag.entryAfter [ "reloadSystemd" ] ''
-    $DRY_RUN_CMD ${lib.getExe' pkgs.systemd "systemctl"} --no-block --user start wallpaper-updater.service
-  '';
-
 }
