83aced5a66561f851bbcc81af97920d2817ff118
Yiheng He <yiheng.he@proton.me>
2025-07-16 17:17:33 +0800

    clash-verge: don't set proxy setttings
    Tun mode is now working

--- home/programs/proxy/clash-verge/default.nix
+++ home/programs/proxy/clash-verge/default.nix
@@ -3,14 +3,4 @@
   home.packages = with pkgs; [
     clash-verge-rev
   ];
-  # HACK: tun mode does not work
-  xdg.configFile."uwsm/env".text = ''
-    export https_proxy=http://127.0.0.1:7897
-    export http_proxy=http://127.0.0.1:7897
-  '';
-  systemd.user.services.hydroxide.Service.Environment = [
-    "export https_proxy=http://127.0.0.1:7897"
-    "export http_proxy=http://127.0.0.1:7897"
-  ];
-
 }
