e684ecd4482876512e2a56d1089972cda758125f
Yiheng He <yiheng.he@proton.me>
2025-07-18 11:21:01 +0800

    caddy: map over hostedServices

--- system/services/caddy.nix
+++ system/services/caddy.nix
@@ -1,7 +1,17 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
-  services.caddy.enable = true;
+  services.caddy = {
+    enable = true;
+    virtualHosts = lib.mapAttrs' (
+      name: port:
+      lib.nameValuePair "${name}.${config.host.address}" {
+        extraConfig = ''
+          reverse_proxy localhost:${toString port}
+        '';
+      }
+    ) config.hostedServices;
+  };
   services.tailscale.permitCertUid = config.services.caddy.user;
   networking.firewall = {
     allowedTCPPorts = [
