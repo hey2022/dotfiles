8b29c3f1d5d51d50affa90d03dadb4feae34c326
Yiheng He <yiheng.he@proton.me>
2025-07-19 21:05:32 +0800

    Update Nix flake inputs (#12)
    * flake: update inputs
    
    Flake lock file updates:
    
    • Updated input 'catppuccin':
        'github:catppuccin/nix/7dc907c010e1612729c5d76cf614b5f7811bfe23?narHash=sha256-CFOuAHbc9PTt9HhjGQFf07bUCZKOahQ%2BvLt30J6u5fw%3D' (2025-07-14)
      → 'github:catppuccin/nix/f5d22072074b0bc3c47a3a96a00e4fad263014ff?narHash=sha256-I8cCVFZIFcIy%2B7KBnjCuS/ISTJYeR8mkah1oxA6qBVk%3D' (2025-07-18)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/bc9f3c8413a3aaa01ed959c371c1f9e57515965b?narHash=sha256-bDgECs6Gls%2BKeIba8ft7jmxM3Hsbc2%2BDseiI4zrra2o%3D' (2025-07-17)
      → 'github:nix-community/home-manager/d0300c8808e41da81d6edfc202f3d3833c157daf?narHash=sha256-irfg7lnfEpJY%2B3Cffkluzp2MTVw1Uq9QGxFp6qadcXI%3D' (2025-07-18)
    • Updated input 'hyprland-contrib':
        'github:hyprwm/contrib/34d0c01910552b873a07c96921ef70e32bf369a2?narHash=sha256-dyzDJvt8IVgHJVmpnw1mueHHSLYnChW1XMkwz9WUBZ8%3D' (2025-07-09)
      → 'github:hyprwm/contrib/481175e17e155f19a3b31416530b6edf725e7034?narHash=sha256-usBNOT/uzFdsKDe5Ik%2BC36zqL%2BBfT7Lp2rqKWrpQuqk%3D' (2025-07-18)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/62e0f05ede1da0d54515d4ea8ce9c733f12d9f08?narHash=sha256-JHQbm%2BOcGp32wAsXTE/FLYGNpb%2B4GLi5oTvCxwSoBOA%3D' (2025-07-14)
      → 'github:NixOS/nixpkgs/6e987485eb2c77e5dcc5af4e3c70843711ef9251?narHash=sha256-RKwfXA4OZROjBTQAl9WOZQFm7L8Bo93FQwSJpAiSRvo%3D' (2025-07-16)
    • Updated input 'nur':
        'github:nix-community/NUR/290ad20d36a3ea1f4ada4964355783890d014608?narHash=sha256-TV3%2BXmq0uTZuNpd70giJvUCqz/H3AbLWKbm3q%2BzfHuk%3D' (2025-07-17)
      → 'github:nix-community/NUR/e6721151f19a4db5099336ea1651201b1ab8023f?narHash=sha256-TJMwY0bKm1SsvpSROg54sglwwpslWYtl1mYh4j%2Bhc1E%3D' (2025-07-18)
    • Updated input 'zen-browser':
        'github:0xc000022070/zen-browser-flake/9aa65a267cba3e8334e3af8ec005e75d9c391bbe?narHash=sha256-VP%2Bf1kJmTsJW%2B62HtJRZw/luhKHaT06lByAI3/6dXSQ%3D' (2025-07-17)
      → 'github:0xc000022070/zen-browser-flake/f19d2b6b18d4a2e8bf2d6a9f69c934d6726360c4?narHash=sha256-oTIVrw7Cy2McAVqE7GCot5Fb8Wh4JBsUDKMX8u3DFlU%3D' (2025-07-18)
    
    * patch: remove "clisp: fix broken symlinks"
    
    * patch: remove "gimp: fix build with gettext 0.25"
    
    * patch: update "gitbutler: 0.14.19 -> 0.15.2" patch
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- lib/patch/default.nix
+++ lib/patch/default.nix
@@ -13,20 +13,8 @@ let
     # https://nixpk.gs/pr-tracker.html?pr=424156
     (pkgs.fetchpatch2 {
       name = "gitbutler: 0.14.19 -> 0.15.2";
-      url = "https://github.com/NixOS/nixpkgs/pull/424156.diff";
-      sha256 = "sha256-eHVynJDZWnaNKakkq1MkdIAr1pxCcMxIW15KGzqNJgo=";
-    })
-    # https://nixpk.gs/pr-tracker.html?pr=425299
-    (pkgs.fetchpatch2 {
-      name = "clisp: fix broken symlinks";
-      url = "https://github.com/NixOS/nixpkgs/pull/425299.patch";
-      sha256 = "sha256-eixt+0nHxoRMMj+Js/wy8E/PWC41L9kgUAasAGH6Ua4=";
-    })
-    # https://nixpk.gs/pr-tracker.html?pr=425710
-    (pkgs.fetchpatch2 {
-      name = "gimp: fix build with gettext 0.25";
-      url = "https://github.com/NixOS/nixpkgs/pull/425710.patch";
-      sha256 = "sha256-KfDo2EABueN2QQVCdQvk2gQV4bgB+LJV3oULqTe05Oo=";
+      url = "https://github.com/NixOS/nixpkgs/pull/424156.patch";
+      sha256 = "sha256-h/3F2k+oOC663rmqoYOQWBkEzaIqeeHrB8lZGUFJdWc=";
     })
     # https://nixpk.gs/pr-tracker.html?pr=404337
     # ./cloudflared.patch
