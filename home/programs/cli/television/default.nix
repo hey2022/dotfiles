a971c2cdb8ff02a7920afc4de93d66574be1cd5b
Yiheng He <yiheng.he@proton.me>
2025-07-20 21:28:10 +0800

    television: add package dependencies

--- home/programs/cli/television/default.nix
+++ home/programs/cli/television/default.nix
@@ -1,8 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.television = {
     enable = true;
   };
+  home.packages = with pkgs; [
+    fd
+    bat
+    nix-search-tv
+  ];
   xdg.configFile."television/cable".source = config.lib.hm.mkFlakeSymlink ./cable;
 }
