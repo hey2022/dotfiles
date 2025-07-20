61cc8140b3d9c3ec09f5a6c3231556dee1f4a0fc
Yiheng He <yiheng.he@proton.me>
2025-07-21 00:39:57 +0800

    wallpapers: create symlink in config

--- home/desktop/wallpaper/default.nix
+++ home/desktop/wallpaper/default.nix
@@ -1,6 +1,8 @@
+{ self, config, ... }:
 {
   imports = [
     ./waypaper.nix
   ];
   services.swww.enable = true;
+  xdg.configFile."wallpapers".source = config.lib.hm.mkFlakeSymlink "${self}/assets/wallpapers";
 }
