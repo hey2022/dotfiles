f6c040cdbcbe6acd74f0ec825a2ee34a2a0659e7
Yiheng He <yiheng.he@proton.me>
2025-07-26 04:39:34 +0800

    desktop: enable entertainment profile

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   imports = [
     ../../home
@@ -10,4 +9,8 @@
   ];
   home.username = "yiheng";
   home.homeDirectory = "/home/yiheng";
+
+  profiles = {
+    entertainment.enable = true;
+  };
 }
