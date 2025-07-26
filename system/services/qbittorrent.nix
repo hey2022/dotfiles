472dd6a8b4fcc297662f20756377b1b05f5852d6
Yiheng He <yiheng.he@proton.me>
2025-07-27 02:45:07 +0800

    qbittorrent: configure to work with media setup
    qbittorrent: set group to media
    
    qbittorrent: set UMask to 002

--- system/services/qbittorrent.nix
+++ system/services/qbittorrent.nix
@@ -5,18 +5,25 @@ let
 in
 {
   services.qbittorrent = {
+    group = "media";
     extraArgs = [
       "--confirm-legal-notice"
     ];
     webuiPort = 8123;
     torrentingPort = 49152;
     serverConfig = {
+      BitTorrent.Session = {
+        DefaultSavePath = "/var/lib/media/downloads";
+      };
       Preferences.WebUI = {
         AlternativeUIEnabled = true;
         RootFolder = "${pkgs.vuetorrent}/share/vuetorrent";
         Password_PBKDF2 = "@ByteArray(XBmouOyHahIz2f4ScM/Plg==:PqXncxjC3/dmAqVkXY3Ga5rgATxmiBjN8xZxEPyuOoBpsK02CmaB2Kmuftbx3NpSrutCzBB0351xI9jJ3CGh/g==)";
       };
     };
+  };
+  systemd.services.qbittorrent.serviceConfig = {
+    UMask = "002";
   };
   networking.firewall.allowedTCPPorts = [ cfg.torrentingPort ];
   homelab.services.qbittorrent = {
