4ed2377cea0c493d5d0d7b5576c4e86d8d28e615
Yiheng He <yiheng.he@proton.me>
2025-07-28 07:59:25 +0800

    prometheus: add to homelab

--- system/services/prometheus/default.nix
+++ system/services/prometheus/default.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ config, ... }:
+
+let
+  cfg = config.services.prometheus;
+in
 {
   imports = [
     ./node_exporter.nix
   ];
+  homelab.services.prometheus = {
+    inherit (cfg) port;
+  };
 }
