c7e6c74d942deb40a7a86f4ac2c0b5014da18995
Yiheng He <yiheng.he@proton.me>
2025-07-30 13:56:38 +0800

    caddy: force disable nginx

--- system/services/homelab/caddy.nix
+++ system/services/homelab/caddy.nix
@@ -1,23 +1,26 @@
 { config, lib, ... }:
 
 {
-  services.caddy = {
-    enable = true;
-    virtualHosts =
-      let
-        domain = config.host.address;
-      in
-      {
-        "${domain}, www.${domain}".extraConfig = ''
-          reverse_proxy localhost:${toString config.homelab.rootService}
-        '';
-        "*.${domain}".extraConfig = lib.concatLines (
-          lib.mapAttrsToList (name: service: ''
-            @${name} host ${service.subdomain}.${domain}
-            reverse_proxy @${name} localhost:${toString service.port}
-          '') (lib.filterAttrs (_: service: service.expose) config.homelab.services)
-        );
-      };
+  services = {
+    caddy = {
+      enable = true;
+      virtualHosts =
+        let
+          domain = config.host.address;
+        in
+        {
+          "${domain}, www.${domain}".extraConfig = ''
+            reverse_proxy localhost:${toString config.homelab.rootService}
+          '';
+          "*.${domain}".extraConfig = lib.concatLines (
+            lib.mapAttrsToList (name: service: ''
+              @${name} host ${service.subdomain}.${domain}
+              reverse_proxy @${name} localhost:${toString service.port}
+            '') (lib.filterAttrs (_: service: service.expose) config.homelab.services)
+          );
+        };
+    };
+    nginx.enable = lib.mkForce false;
   };
   services.tailscale.permitCertUid = config.services.caddy.user;
   networking.firewall = {
