d9c831d3246346479809b5148ebda562ff111ceb
Yiheng He <yiheng.he@proton.me>
2025-07-30 11:59:57 +0800

    cloudflared: increase QUIC UDP buffer size

--- system/services/homelab/cloudflared.nix
+++ system/services/homelab/cloudflared.nix
@@ -6,5 +6,9 @@ in
 {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ cfg.package ];
+    boot.kernel.sysctl = {
+      "net.core.rmem_max" = 7500000;
+      "net.core.wmem_max" = 7500000;
+    };
   };
 }
