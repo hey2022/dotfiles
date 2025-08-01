3c95d1646417815fcbd493cd9038a06e101ec3e1
Yiheng He <yiheng.he@proton.me>
2025-08-01 22:22:53 +0800

    treewide: set opacity to 0.9

--- common/stylix.nix
+++ common/stylix.nix
@@ -36,10 +36,10 @@ in
       size = 24;
     };
     opacity = {
-      terminal = 0.5;
-      applications = 0.5;
-      desktop = 0.5;
-      popups = 0.5;
+      terminal = 0.9;
+      applications = 0.9;
+      desktop = 0.9;
+      popups = 0.9;
     };
   };
 }
