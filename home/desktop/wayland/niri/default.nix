c2483be8e25778656228efa05a7c4023c425d23f
Yiheng He <yiheng.he@proton.me>
2025-08-01 21:29:56 +0800

    niri: enable programs

--- home/desktop/wayland/niri/default.nix
+++ home/desktop/wayland/niri/default.nix
@@ -1,7 +1,25 @@
-{ config, ... }:
+{ config, lib, ... }:
 
+let
+  cfg = config.programs.niri;
+in
 {
-  programs.niri = {
-    configFile = config.lib.hm.mkFlakeSymlink ./config.kdl;
+  config = lib.mkIf cfg.enable {
+    programs.niri = {
+      configFile = config.lib.hm.mkFlakeSymlink ./config.kdl;
+    };
+    programs = {
+      rofi.enable = true;
+      ghostty.enable = true;
+      hyprlock.enable = true;
+      waybar.enable = true;
+      wlogout.enable = true;
+    };
+    services = {
+      cliphist.enable = true;
+      gammastep.enable = true;
+      hypridle.enable = true;
+      swaync.enable = true;
+    };
   };
 }
