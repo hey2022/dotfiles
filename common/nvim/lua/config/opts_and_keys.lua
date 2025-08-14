d3b664bfb094ab2781cab0c39331f8583febbb5c
Yiheng He <yiheng.he@proton.me>
2025-08-14 18:32:45 +0800

    neovim/opts: configure indentation

--- common/nvim/lua/config/opts_and_keys.lua
+++ common/nvim/lua/config/opts_and_keys.lua
@@ -29,14 +29,14 @@ vim.wo.number = true
 vim.o.mouse = "a"
 
 -- Indent
--- vim.o.smarttab = true
+vim.o.smarttab = true
 vim.opt.cpoptions:append("I")
 vim.o.expandtab = true
--- vim.o.smartindent = true
--- vim.o.autoindent = true
--- vim.o.tabstop = 4
--- vim.o.softtabstop = 4
--- vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 -- stops line wrapping from being confusing
 vim.o.breakindent = true
