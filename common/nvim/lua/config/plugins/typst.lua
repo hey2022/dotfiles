ca8b74711b3dff4eca0d9052dbbbe8bfa9be621a
Yiheng He <yiheng.he@proton.me>
2025-08-19 02:27:36 +0800

    neovim/typst: fix typst-preview.nvim filetype

--- common/nvim/lua/config/plugins/typst.lua
+++ common/nvim/lua/config/plugins/typst.lua
@@ -2,7 +2,7 @@ return {
     {
         "typst-preview.nvim",
         for_cat = "typst",
-        ft = "typ",
+        ft = "typst",
         after = function(_)
             require("typst-preview").setup({})
             vim.keymap.set("n", "<leader>tp", "<cmd>TypstPreview<CR>", { noremap = true, desc = "typst preview" })
