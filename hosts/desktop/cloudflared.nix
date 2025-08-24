b4588ebb7824731ad91df65b3308a5fd417736ed
Yiheng He <yiheng.he@proton.me>
2025-08-25 01:46:44 +0800

    caddy: update clouadflare dns plugin hash

--- hosts/desktop/cloudflared.nix
+++ hosts/desktop/cloudflared.nix
@@ -24,7 +24,7 @@
     caddy = {
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-        hash = "sha256-2D7dnG50CwtCho+U+iHmSj2w14zllQXPjmTHr6lJZ/A=";
+        hash = "sha256-S1JN7brvH2KIu7DaDOH1zij3j8hWLLc0HdnUc+L89uU=";
       };
       environmentFile = config.sops.secrets.caddy.path;
       globalConfig = ''
