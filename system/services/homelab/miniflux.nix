0c909be0c69197c3c70e3820edf853e236093c26
Yiheng He <yiheng.he@proton.me>
2025-08-25 02:05:48 +0800

    miniflux: disable

--- system/services/homelab/miniflux.nix
+++ system/services/homelab/miniflux.nix
@@ -3,7 +3,6 @@ let
 in
 {
   services.miniflux = {
-    enable = true;
     adminCredentialsFile = /etc/nixos/miniflux-admin-credentials;
     config = {
       LISTEN_ADDR = "0.0.0.0:${toString port}";
