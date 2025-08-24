5901b020b7fb85d7847851723e7d65f66594b965
Yiheng He <yiheng.he@proton.me>
2025-08-25 03:12:14 +0800

    zjs: add zoxide argument support

--- home/shell/scripts/zjs/zjs.sh
+++ home/shell/scripts/zjs/zjs.sh
@@ -4,7 +4,12 @@ if [[ -n "${ZELLIJ-}" ]]; then
     exit 0
 fi
 
-dir=$(zoxide query --interactive)
+if [[ -z $1 ]]; then
+    dir=$(zoxide query --interactive)
+else
+    dir=$(zoxide query "$1")
+fi
+
 if [[ -z "$dir" ]]; then
     exit 0
 fi
