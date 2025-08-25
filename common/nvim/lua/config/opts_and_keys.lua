2126318cd08a9e1b37bb8edd3a9d2db5607fc2e9
Yiheng He <yiheng.he@proton.me>
2025-08-26 02:20:00 +0800

    neovim/opts_and_keys: remap diagnostic keys

--- common/nvim/lua/config/opts_and_keys.lua
+++ common/nvim/lua/config/opts_and_keys.lua
@@ -115,8 +115,8 @@ vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>cD", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- kickstart.nvim starts you with this.
 -- But it constantly clobbers your system clipboard whenever you delete anything.
