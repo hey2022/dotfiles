5cdb2ce775ff520a055d856bd3dc9572b28a98f6
Yiheng He <yiheng.he@proton.me>
2025-09-01 00:54:52 +0800

    zettelkasten: obsidian use xwayland

--- home/programs/productivity/zettelkasten.nix
+++ home/programs/productivity/zettelkasten.nix
@@ -10,5 +10,13 @@
     home.packages = with pkgs; [
       obsidian
     ];
+    xdg.desktopEntries."obsidian" = {
+      name = "Obsidian";
+      comment = "Knowledge base";
+      icon = "obsidian";
+      exec = "env -u WAYLAND_DISPLAY obsidian %u";
+      categories = [ "Office" ];
+      mimeType = [ "x-scheme-handler/obsidian" ];
+    };
   };
 }
