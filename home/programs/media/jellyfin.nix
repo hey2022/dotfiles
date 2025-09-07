d58ec4419d67fdd6e7380e046a4aec9fa9cf5e87
Yiheng He <yiheng.he@proton.me>
2025-09-07 16:44:22 +0800

    jellyfin: permit insecure qtwebengine

--- home/programs/media/jellyfin.nix
+++ home/programs/media/jellyfin.nix
@@ -5,9 +5,11 @@
   ...
 }:
 {
-  home.packages =
-    with pkgs;
-    lib.mkIf config.profiles.entertainment.enable [
-      jellyfin-media-player
+  config = lib.mkIf config.profiles.entertainment.enable {
+    home.packages = with pkgs; [ jellyfin-media-player ];
+    # HACK: https://github.com/nixos/nixpkgs/issues/437865
+    nixpkgs.config.permittedInsecurePackages = [
+      "qtwebengine-5.15.19"
     ];
+  };
 }
