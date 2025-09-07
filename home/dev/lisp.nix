71d6ab71130bd5f970070705da6faa62e2cba775
Yiheng He <yiheng.he@proton.me>
2025-09-07 23:55:23 +0800

    dev/lisp: create lisp dev profile

--- home/dev/lisp.nix
+++ home/dev/lisp.nix
@@ -1,7 +1,19 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    sbcl
-    rlwrap
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.profiles.dev.lisp;
+in
+{
+  options.profiles.dev.lisp.enable = lib.mkEnableOption "lisp dev profile";
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      sbcl
+      rlwrap
+    ];
+  };
 }
