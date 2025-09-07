034910578655f053a94ad690a8e76da7802141e5
Yiheng He <yiheng.he@proton.me>
2025-09-07 23:50:02 +0800

    dev/android: create android dev profile

--- home/dev/android.nix
+++ home/dev/android.nix
@@ -1,6 +1,18 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    qtscrcpy
-  ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.profiles.dev.android;
+in
+{
+  options.profiles.dev.android.enable = lib.mkEnableOption "android dev profile";
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      qtscrcpy
+    ];
+  };
 }
