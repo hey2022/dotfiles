e0ba81a95ac30e8831974b477709c36b0e2d0a78
Yiheng He <yiheng.he@proton.me>
2025-09-07 23:57:00 +0800

    dev/julia: create julia dev profile

--- home/dev/julia.nix
+++ home/dev/julia.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [ julia ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.profiles.dev.julia;
+in
+{
+  options.profiles.dev.julia.enable = lib.mkEnableOption "julia dev profile";
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [ julia ];
+  };
 }
