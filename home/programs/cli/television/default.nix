49a4bb78f1011bdf8abd6a96bd6449d0cfe9cb39
Yiheng He <yiheng.he@proton.me>
2025-10-16 10:00:10 +0800

    television: remove television overlay

--- home/programs/cli/television/default.nix
+++ home/programs/cli/television/default.nix
@@ -1,28 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  # TODO: https://nixpk.gs/pr-tracker.html?pr=423662
-  television-overlay = _: prev: {
-    television = prev.television.overrideAttrs (
-      finalAttrs: _: {
-        version = "0.12.4";
-        src = prev.fetchFromGitHub {
-          owner = "alexpasmantier";
-          repo = "television";
-          tag = finalAttrs.version;
-          hash = "sha256-J4z0QKw4P2auIbp6SU+XsA/hCJJCN5WUIVwZJAICSrs=";
-        };
-        cargoDeps = prev.rustPlatform.fetchCargoVendor {
-          inherit (finalAttrs) src;
-          hash = "sha256-ASJ3QXe4AqEtTdezwWvWvTIdKazQv+1Hr9gcjG6HcsE=";
-        };
-        doCheck = false;
-      }
-    );
-  };
-in
 {
-  nixpkgs.overlays = [ television-overlay ];
   programs.television = {
     enable = true;
   };
