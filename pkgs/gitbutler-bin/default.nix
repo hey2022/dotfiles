5b8297e05f621b92f0fc79b7dda08ef8d61ec7e6
Yiheng He <yiheng.he@proton.me>
2025-10-26 10:30:44 +0800

    pkgs/gitbutler-bin: fix TLS

--- pkgs/gitbutler-bin/default.nix
+++ pkgs/gitbutler-bin/default.nix
@@ -4,31 +4,29 @@
   fetchurl,
   dpkg,
   autoPatchelfHook,
-  libgit2,
-  openssl,
   glib-networking,
   webkitgtk_4_1,
+  makeWrapper,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gitbutler-bin";
   version = "0.16.10";
 
   src = fetchurl {
-    url = "https://releases.gitbutler.com/releases/release/0.16.10-2482/linux/x86_64/GitButler_0.16.10_amd64.deb";
+    url = "https://releases.gitbutler.com/releases/release/${finalAttrs.version}-2482/linux/x86_64/GitButler_0.16.10_amd64.deb";
     hash = "sha256-5XQxDr1sgwIQc4sAMXP5grhYY8p5k8BFRZynim+Pu38=";
   };
 
   nativeBuildInputs = [
     dpkg
     autoPatchelfHook
+    makeWrapper
   ];
 
   buildInputs = [
-    libgit2
-    openssl
-    glib-networking
     webkitgtk_4_1
+    glib-networking
   ];
 
   installPhase = ''
@@ -36,6 +34,9 @@ stdenv.mkDerivation {
 
     mkdir -p $out
     cp -r usr/* $out/
+
+    wrapProgram "$out/bin/${finalAttrs.meta.mainProgram}" \
+      --prefix GIO_EXTRA_MODULES : "${glib-networking}/lib/gio/modules"
 
     runHook postInstall
   '';
@@ -46,4 +47,4 @@ stdenv.mkDerivation {
     license = lib.licenses.fsl11Mit;
     mainProgram = "gitbutler-tauri";
   };
-}
+})
