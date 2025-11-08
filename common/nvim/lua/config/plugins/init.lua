869b86cb298cc98c990f8a6fe27a84c52096a1ad
Yiheng He <yiheng.he@proton.me>
2025-11-09 05:11:04 +0800

    neovim/which-key: remove hidden keymaps

--- common/nvim/lua/config/plugins/init.lua
+++ common/nvim/lua/config/plugins/init.lua
@@ -312,19 +312,12 @@ require("lze").load({
             require("which-key").setup({})
             require("which-key").add({
                 { "<leader><leader>", group = "buffer commands" },
-                { "<leader><leader>_", hidden = true },
                 { "<leader>c", group = "[c]ode" },
-                { "<leader>c_", hidden = true },
                 { "<leader>g", group = "[g]it" },
-                { "<leader>g_", hidden = true },
                 { "<leader>m", group = "[m]arkdown" },
-                { "<leader>m_", hidden = true },
                 { "<leader>s", group = "[s]earch" },
-                { "<leader>s_", hidden = true },
                 { "<leader>t", group = "[t]oggles" },
-                { "<leader>t_", hidden = true },
                 { "<leader>w", group = "[w]orkspace" },
-                { "<leader>w_", hidden = true },
             })
         end,
     },
