a5615891dc2fa66b2f2ebe5e23ffc1be622d6ba1
Yiheng He <yiheng.he@proton.me>
2025-11-11 18:05:54 +0800

    dictionary: add xxhash and beautifulsoup4 to pyglossary

--- home/programs/dictionary.nix
+++ home/programs/dictionary.nix
@@ -1,7 +1,22 @@
 { pkgs, ... }:
+
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      python3 = prev.python3.override {
+        packageOverrides = python-final: python-prev: {
+          pyglossary = python-prev.pyglossary.overrideAttrs (oldAttrs: {
+            propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
+              python-final.xxhash
+              python-final.beautifulsoup4
+            ];
+          });
+        };
+      };
+    })
+  ];
   home.packages = with pkgs; [
-    pkgs.goldendict-ng
+    goldendict-ng
     pyglossary-gui
   ];
 }
