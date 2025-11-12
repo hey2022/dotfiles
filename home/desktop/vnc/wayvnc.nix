46164b805c770972d1add3980aeece0c171d5ff0
Yiheng He <yiheng.he@proton.me>
2025-11-12 10:54:09 +0800

    wayvnc: enable

--- home/desktop/vnc/wayvnc.nix
+++ home/desktop/vnc/wayvnc.nix
@@ -1,28 +1,10 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  options = {
-    wayvnc.enable = lib.mkEnableOption "Enable wayvnc";
-  };
-  config = lib.mkIf config.wayvnc.enable {
-    home.packages = with pkgs; [ wayvnc ];
-    systemd.user.services.wayvnc = {
-      Unit = {
-        Description = "Start wayvnc";
-      };
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.wayvnc}/bin/wayvnc -r";
-        Restart = "always";
-        RestartSec = 15;
-      };
+  services.wayvnc = {
+    enable = true;
+    autoStart = true;
+    settings = {
+      address = "localhost";
+      port = 5901;
     };
   };
 }
