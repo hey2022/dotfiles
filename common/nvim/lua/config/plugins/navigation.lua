9e6b0dbb4bff792d72a5d07da84d12ba509c5e34
Yiheng He <yiheng.he@proton.me>
2025-11-15 02:38:21 +0800

    neovim/navigation/aerial: init

--- common/nvim/lua/config/plugins/navigation.lua
+++ common/nvim/lua/config/plugins/navigation.lua
@@ -55,4 +55,26 @@ return {
             },
         },
     },
+    {
+        "aerial.nvim",
+        for_cat = "general.extra",
+        after = function(_)
+            require("aerial").setup({
+                on_attach = function(bufnr)
+                    -- Jump forwards/backwards with '{' and '}'
+                    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                end,
+            })
+        end,
+        keys = {
+            {
+                "<leader>co",
+                function()
+                    require("aerial").toggle()
+                end,
+                desc = "Toggle aerial outline",
+            },
+        },
+    },
 }
