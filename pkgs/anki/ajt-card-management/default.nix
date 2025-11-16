74ca1f65507abadfeae2eb9db05a3ab028aa9843
Yiheng He <yiheng.he@proton.me>
2025-11-17 03:30:20 +0800

    pkgs/anki/ajt-card-management: add ssh workaround

--- pkgs/anki/ajt-card-management/default.nix
+++ pkgs/anki/ajt-card-management/default.nix
@@ -8,13 +8,21 @@
 anki-utils.buildAnkiAddon (finalAttrs: {
   pname = "ajt-card-management";
   version = "25.10.14.0";
-  src = fetchFromGitHub {
-    owner = "Ajatt-Tools";
-    repo = "learn-now-button";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-ebGMrEfDV9ZWtrV2AjiaNd7WMeNBHlaOBE2xL1x0nWs=";
-    fetchSubmodules = true;
-  };
+  src =
+    (fetchFromGitHub {
+      owner = "Ajatt-Tools";
+      repo = "learn-now-button";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-ebGMrEfDV9ZWtrV2AjiaNd7WMeNBHlaOBE2xL1x0nWs=";
+      fetchSubmodules = true;
+    }).overrideAttrs
+      (oldAttrs: {
+        env = oldAttrs.env or { } // {
+          GIT_CONFIG_COUNT = 1;
+          GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+          GIT_CONFIG_VALUE_0 = "git@github.com:";
+        };
+      });
   sourceRoot = "${finalAttrs.src.name}/card_management";
   passthru.updateScript = nix-update-script { };
   meta = {
