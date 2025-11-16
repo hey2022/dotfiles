96bec46ee02bbbd999896135fc8dae7bc80e0b4e
Yiheng He <yiheng.he@proton.me>
2025-11-17 03:42:26 +0800

    searx: use startpage over mullvad leta

--- system/services/searx.nix
+++ system/services/searx.nix
@@ -10,10 +10,8 @@
         port = 8880;
       };
       engines = lib.mapAttrsToList (name: value: { inherit name; } // value) {
-        "startpage".disabled = true;
         # BUG: https://github.com/searxng/searxng/issues/5286
         "google".disabled = true;
-        "mullvadleta".disabled = false;
         # BUG: Wikidata breaks all searches
         "wikidata".disabled = true;
       };
