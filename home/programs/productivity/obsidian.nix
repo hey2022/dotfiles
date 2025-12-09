46ebca932e1163e14626c6ec4b478beb9b2d3595
Yiheng He <yiheng.he@proton.me>
2025-12-09 15:00:59 +0800

    obisidan: force use x11

--- home/programs/productivity/obsidian.nix
+++ home/programs/productivity/obsidian.nix
@@ -10,5 +10,13 @@
     home.packages = with pkgs; [
       obsidian
     ];
+    xdg.desktopEntries."obsidian" = {
+      name = "Obsidian";
+      comment = "Knowledge base";
+      icon = "obsidian";
+      exec = "obsidian --ozone-platform=x11 %u";
+      categories = [ "Office" ];
+      mimeType = [ "x-scheme-handler/obsidian" ];
+    };
   };
 }
