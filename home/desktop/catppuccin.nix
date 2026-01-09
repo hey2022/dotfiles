887a4800b8f08832c4f5755bc93ecd4af825dc52
Yiheng He <yiheng.he@proton.me>
2026-01-09 12:10:20 +0800

    fcitx5: don't apply catppuccin

--- home/desktop/catppuccin.nix
+++ home/desktop/catppuccin.nix
@@ -4,6 +4,7 @@
   catppuccin = {
     enable = true;
     flavor = "mocha";
+    fcitx5.apply = false;
     hyprlock.useDefaultConfig = false;
     wezterm.apply = true;
   };
--- home/desktop/fcitx5.nix
+++ home/desktop/fcitx5.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   i18n.inputMethod = {
     enable = true;
@@ -6,6 +6,8 @@
     fcitx5 = {
       addons = with pkgs; [ qt6Packages.fcitx5-chinese-addons ];
       waylandFrontend = true;
+      # HACK: https://github.com/catppuccin/nix/pull/820
+      settings.addons = lib.mkForce { };
     };
   };
   home.sessionVariables = {
