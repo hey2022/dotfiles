98dac17ea53b7743746615a29ffe207b4f192e41
Yiheng He <yiheng.he@proton.me>
2026-01-13 17:43:25 +0800

    neovim/vimtex: fix synctex inverse search

--- common/nvim/lua/config/plugins/latex.lua
+++ common/nvim/lua/config/plugins/latex.lua
@@ -4,6 +4,7 @@ return {
         for_cat = "languages.latex",
         after = function(_)
             vim.g.vimtex_view_method = "sioyek"
+            vim.g.vimtex_callback_progpath = "nvim"
         end,
     },
 }
