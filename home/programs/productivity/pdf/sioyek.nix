60fa210bc2f511b975f8f75eea6dbacd0e92857a
Yiheng He <yiheng.he@proton.me>
2026-01-13 20:04:36 +0800

    pdf: use sioyek instead of zathura

---
+++ home/programs/productivity/pdf/sioyek.nix
@@ -0,0 +1,15 @@
+{
+  programs.sioyek = {
+    config = {
+      "should_launch_new_window" = "1";
+    };
+    bindings = {
+      "screen_down" = "J";
+      "screen_up" = "K";
+      "zoom_in" = "+";
+      "fit_to_page_width_smart" = "=";
+      "prev_state" = "<C-o>";
+      "next_state" = "<C-i>";
+    };
+  };
+}
