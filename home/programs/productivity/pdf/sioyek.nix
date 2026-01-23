f55ffcf9924cd969c0a075c1b53589ebf354847e
Yiheng He <yiheng.he@proton.me>
2026-01-23 09:05:11 +0800

    sioyek: bind = to fit_to_page_width

--- home/programs/productivity/pdf/sioyek.nix
+++ home/programs/productivity/pdf/sioyek.nix
@@ -7,7 +7,7 @@
       "screen_down" = "J";
       "screen_up" = "K";
       "zoom_in" = "+";
-      "fit_to_page_width_smart" = "=";
+      "fit_to_page_width" = "=";
       "prev_state" = "<C-o>";
       "next_state" = "<C-i>";
     };
