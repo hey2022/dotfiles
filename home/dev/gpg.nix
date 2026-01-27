5fee6b7789b78df16cd19b347ccc808b0864be0a
Yiheng He <yiheng.he@proton.me>
2026-01-27 09:48:20 +0800

    gpg: add gpg-agent debugging comment

--- home/dev/gpg.nix
+++ home/dev/gpg.nix
@@ -3,6 +3,7 @@
   programs.gpg = {
     enable = true;
   };
+  # If no pinentry run `gpgconf --reload gpg-agent`
   services.gpg-agent = {
     enable = true;
     pinentry.package = pkgs.pinentry-qt;
