6c7deb3b0b54943a56fd2e06068872c9730d1b0b
Yiheng He <yiheng.he@proton.me>
2026-01-29 00:07:25 +0800

    tridactyl: dont import emacs

--- home/programs/browsers/firefox/extensions/tridactyl/default.nix
+++ home/programs/browsers/firefox/extensions/tridactyl/default.nix
@@ -1,11 +1,5 @@
+{ config, ... }:
+
 {
-  inputs,
-  config,
-  ...
-}:
-{
-  imports = [
-    "${inputs.self}/home/dev/emacs"
-  ];
   xdg.configFile."tridactyl/tridactylrc".source = config.lib.hm.mkFlakeSymlink ./tridactylrc;
 }
