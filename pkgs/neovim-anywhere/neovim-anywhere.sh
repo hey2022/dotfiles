ab56e30fd3b7df10fff31ef6b98a3c8d0c35d86a
Yiheng He <yiheng.he@proton.me>
2026-01-29 22:55:18 +0800

    neovim-anywhere: fix paste on exit

--- pkgs/neovim-anywhere/neovim-anywhere.sh
+++ pkgs/neovim-anywhere/neovim-anywhere.sh
@@ -9,5 +9,4 @@ ft="${1:-markdown}"
 
 neovide -- -c "set filetype=${ft}" "$tmp"
 
-[ -s "$tmp" ] && wl-copy < "$tmp"
-ydotool key 29:1 47:1 47:0 29:0
+[ -s "$tmp" ] && wl-copy < "$tmp" && ydotool key 29:1 47:1 47:0 29:0
