f492dc633ad773679d2026f860fd1114c29dbded
Yiheng He <yiheng.he@proton.me>
2026-02-03 07:03:04 +0800

    power/tlp: extend battery runtime

--- system/power/tlp.nix
+++ system/power/tlp.nix
@@ -8,8 +8,17 @@
     enable = true;
     settings = {
       # https://linrunner.de/tlp/support/optimizing.html
+      # Extend battery runtime
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_SAV = 0;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_SAV = 0;
+      AMDGPU_ABM_LEVEL_ON_BAT = 3;
+
+      # Improve performance on AC power
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_AC = "performance";
 
       # https://linrunner.de/tlp/settings/battery.html
       START_CHARGE_THRESH_BAT0 = 90;
