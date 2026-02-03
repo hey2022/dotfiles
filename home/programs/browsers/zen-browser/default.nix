cd0eca2dcc50f1669acebea44acf722a8cd6f741
Yiheng He <yiheng.he@proton.me>
2026-02-03 18:47:30 +0800

    browsers: remove untrap for youtube

--- home/programs/browsers/zen-browser/default.nix
+++ home/programs/browsers/zen-browser/default.nix
@@ -57,7 +57,6 @@ in
               translate-web-pages
               tridactyl
               ublock-origin
-              untrap-for-youtube
               violentmonkey
               yomitan
               zotero-connector
