410f86c6b7acf2a3644f571e4a83cb2725e8298e
Yiheng He <yiheng.he@proton.me>
2026-02-05 09:32:01 +0800

    power/tlp: disable batter power optimization

--- system/power/tlp.nix
+++ system/power/tlp.nix
@@ -9,13 +9,13 @@
     settings = {
       # https://linrunner.de/tlp/support/optimizing.html
       # Extend battery runtime
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
-      CPU_BOOST_ON_BAT = 0;
-      CPU_BOOST_ON_SAV = 0;
-      CPU_HWP_DYN_BOOST_ON_BAT = 0;
-      CPU_HWP_DYN_BOOST_ON_SAV = 0;
-      AMDGPU_ABM_LEVEL_ON_BAT = 3;
+      # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      # PLATFORM_PROFILE_ON_BAT = "low-power";
+      # CPU_BOOST_ON_BAT = 0;
+      # CPU_BOOST_ON_SAV = 0;
+      # CPU_HWP_DYN_BOOST_ON_BAT = 0;
+      # CPU_HWP_DYN_BOOST_ON_SAV = 0;
+      # AMDGPU_ABM_LEVEL_ON_BAT = 3;
 
       # Improve performance on AC power
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
