50c07dc61bdda348545f93a222207a47e7e69c63
Yiheng He <yiheng.he@proton.me>
2026-03-13 21:59:40 +0800

    wallpaper/linux-wallpaper-engine: init

--- flake.nix
+++ flake.nix
@@ -67,6 +67,10 @@
       url = "github:unmojang/FjordLauncher";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    linux-wallpaper-engine = {
+      url = "github:jagrat7/linux-wallpaper-engine";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     firefox-ui-fix = {
       url = "github:black7375/Firefox-UI-Fix";
       flake = false;
---
+++ home/desktop/wallpaper/swww.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  config = lib.mkIf config.services.swww.enable {
+    home.packages = with pkgs; [
+      waypaper
+    ];
+    systemd.user = {
+      services = {
+        waypaper-random = {
+          Unit = {
+            Description = "Waypaper next random wallpaper";
+          };
+          Service = {
+            Type = "oneshot";
+            ExecStart = "${lib.getExe pkgs.waypaper} --random";
+          };
+        };
+      };
+      timers = {
+        waypaper-wallpaper-cycle = {
+          Unit.Description = "Waypaper random wallpaper cycle";
+          Timer = {
+            Unit = "waypaper-random.service";
+            OnCalendar = "daily";
+            Persistent = true;
+          };
+          Install.WantedBy = [ "timers.target" ];
+        };
+      };
+    };
+  };
+}
