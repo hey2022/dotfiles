137e89fd773cdf33ed8468f0b4e58c23627150a4
Yiheng He <yiheng.he@proton.me>
2026-03-16 01:41:28 +0800

    clash-verge: remove home-manager package

--- home/programs/proxy/clash-verge/default.nix
+++ home/programs/proxy/clash-verge/default.nix
@@ -1,9 +1,6 @@
-{ config, pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    clash-verge-rev
-  ];
+{ config, ... }:
 
+{
   xdg.dataFile."io.github.clash-verge-rev.clash-verge-rev/profiles/Merge.yaml".source =
     config.lib.hm.mkFlakeSymlink ./Merge.yaml;
   xdg.dataFile."io.github.clash-verge-rev.clash-verge-rev/profiles/Script.js".source =
