f3eb18f40991ecd8d4da84df8ead060ad00799b2
Yiheng He <yiheng.he@proton.me>
2026-03-20 09:32:17 +0800

    searx: disable

--- system/services/searx.nix
+++ system/services/searx.nix
@@ -1,25 +1,28 @@
 { config, lib, ... }:
 
+let
+  cfg = config.services.searx;
+in
 {
-  services.searx = {
-    enable = true;
-    environmentFile = config.sops.secrets.searx.path;
-    settings = {
-      server = {
-        bind_address = "127.0.0.1";
-        port = 8880;
-      };
-      engines = lib.mapAttrsToList (name: value: { inherit name; } // value) {
-        # BUG: https://github.com/searxng/searxng/issues/5286
-        "google".disabled = true;
-        # BUG: Wikidata breaks all searches
-        "wikidata".disabled = true;
+  config = lib.mkIf cfg.enable {
+    services.searx = {
+      environmentFile = config.sops.secrets.searx.path;
+      settings = {
+        server = {
+          bind_address = "127.0.0.1";
+          port = 8880;
+        };
+        engines = lib.mapAttrsToList (name: value: { inherit name; } // value) {
+          # BUG: https://github.com/searxng/searxng/issues/5286
+          "google".disabled = true;
+          # BUG: Wikidata breaks all searches
+          "wikidata".disabled = true;
+        };
       };
     };
-  };
-
-  sops.secrets.searx = {
-    sopsFile = ../../secrets/root/searx.env;
-    format = "dotenv";
+    sops.secrets.searx = {
+      sopsFile = ../../secrets/root/searx.env;
+      format = "dotenv";
+    };
   };
 }
