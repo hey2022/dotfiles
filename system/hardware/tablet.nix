669e32ccbe6fb4185c9d2390391f0042c1981699
Yiheng He <yiheng.he@proton.me>
2026-04-08 14:00:17 +0800

    hardware/tablet: disable otd as it doesn't support wireless

--- system/hardware/tablet.nix
+++ system/hardware/tablet.nix
@@ -1,3 +1,4 @@
 {
-  hardware.opentabletdriver.enable = true;
+  # BUG: Disabled until https://github.com/OpenTabletDriver/OpenTabletDriver/issues/2118 is fixed
+  hardware.opentabletdriver.enable = false;
 }
