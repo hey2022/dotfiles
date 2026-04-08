0ea4faa7bfd6dd6e504bdd8ef6d8f8957724a6fe
Yiheng He <yiheng.he@proton.me>
2026-04-08 17:41:01 +0800

    hardware/fan: enable thinkfan

--- system/hardware/thinkpad.nix
+++ system/hardware/thinkpad.nix
@@ -7,7 +7,8 @@ in
     hardware.thinkpad.enable = lib.mkEnableOption "thinkpad support";
   };
   config = lib.mkIf cfg.enable {
-    # control fan speed through /proc/acpi/ibm/fan
-    boot.extraModprobeConfig = "options thinkpad_acpi experimental=1 fan_control=1";
+    services = {
+      thinkfan.enable = true;
+    };
   };
 }
