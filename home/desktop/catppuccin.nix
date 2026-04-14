85ed4e192b97dbbb5606cef752c589546d647042
Yiheng He <yiheng.he@proton.me>
2026-04-14 10:06:56 +0800

    tmux: use catppuccin theme

--- home/desktop/catppuccin.nix
+++ home/desktop/catppuccin.nix
@@ -7,5 +7,12 @@
     fcitx5.apply = false;
     hyprlock.useDefaultConfig = false;
     wezterm.apply = true;
+    tmux.extraConfig = ''
+      set -g @catppuccin_window_status_style "rounded"
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_host}"
+    '';
   };
 }
