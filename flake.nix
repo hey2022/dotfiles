14557fcc56758d747fe6341421de0a4c90fbc01a
Yiheng He <yiheng.he@proton.me>
2026-04-21 15:25:01 +0800

    flake: update nixpkgs-stable 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
