f853954d9e876563a5550a4e4130b016632a5231
Yiheng He <yiheng.he@proton.me>
2026-05-30 00:13:49 +0800

    virtualisation/waydroid: init with x1c12

---
+++ system/virtualisation/waydroid.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.virtualisation.waydroid;
+in
+{
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+      nur.repos.ataraxiasjel.waydroid-script
+    ];
+  };
+
+}
