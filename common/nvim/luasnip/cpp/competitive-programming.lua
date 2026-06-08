defd577b3d55e489b4de496ea41fc17ba6384b9f
Yiheng He <yiheng.he@proton.me>
2026-06-08 20:05:02 +0800

    nvim/luasnip: add ; prefix to all cp autosnippets

--- common/nvim/luasnip/cpp/competitive-programming.lua
+++ common/nvim/luasnip/cpp/competitive-programming.lua
@@ -5,8 +5,15 @@ local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
+local function w(context, nodes, opts)
+    if type(context) ~= "string" and context.trig and context.snippetType == "autosnippet" then
+        context.trig = ";" .. context.trig
+    end
+    return s(context, nodes, opts)
+end
+
 ls.add_snippets("cpp", {
-    s(
+    w(
         "cpt",
         fmt(
             [[
@@ -33,19 +40,19 @@ ls.add_snippets("cpp", {
             i(0)
         )
     ),
-    s("d4", {
+    w("d4", {
         t({
             "const int dx[] = { 0,  1,  0, -1};",
             "const int dy[] = {-1,  0,  1,  0};",
         }),
     }),
-    s("d8", {
+    w("d8", {
         t({
             "const int dx[] = { 0,  1,  1,  1,  0, -1, -1, -1};",
             "const int dy[] = {-1, -1,  0,  1,  1,  1,  0, -1};",
         }),
     }),
-    s(
+    w(
         "bounds_check",
         fmt(
             [[
@@ -64,29 +71,29 @@ ls.add_snippets("cpp", {
             }
         )
     ),
-    s({ trig = "ll", snippetType = "autosnippet" }, t("long long")),
-    s({ trig = "ull", snippetType = "autosnippet" }, t("unsigned long long")),
-    s({ trig = "dbl", snippetType = "autosnippet" }, t("double")),
-    s({ trig = "ldb", snippetType = "autosnippet" }, t("long double")),
-    s({ trig = "str ", snippetType = "autosnippet" }, t("string ")),
+    w({ trig = "ll", snippetType = "autosnippet" }, t("long long")),
+    w({ trig = "ull", snippetType = "autosnippet" }, t("unsigned long long")),
+    w({ trig = "dbl", snippetType = "autosnippet" }, t("double")),
+    w({ trig = "ldb", snippetType = "autosnippet" }, t("long double")),
+    w({ trig = "str", snippetType = "autosnippet" }, t("string ")),
 
-    s({ trig = "pair", snippetType = "autosnippet" }, fmt("pair<{}, {}> {}", { i(1), i(2), i(0) })),
-    s({ trig = "arr", snippetType = "autosnippet" }, fmt("arr<{}> {}", { i(1), i(0) })),
-    s({ trig = "vec", snippetType = "autosnippet" }, fmt("vector<{}> {}", { i(1), i(0) })),
-    s({ trig = "vi", snippetType = "autosnippet" }, t("vector<int>")),
-    s({ trig = "vl", snippetType = "autosnippet" }, t("vector<long long>")),
-    s({ trig = "vd", snippetType = "autosnippet" }, t("vector<long double>")),
-    s({ trig = "vb", snippetType = "autosnippet" }, t("vector<bool>")),
-    s({ trig = "vs", snippetType = "autosnippet" }, t("vector<string>")),
-    s({ trig = "vc", snippetType = "autosnippet" }, t("vector<char>")),
+    w({ trig = "pair", snippetType = "autosnippet" }, fmt("pair<{}, {}> {}", { i(1), i(2), i(0) })),
+    w({ trig = "arr", snippetType = "autosnippet" }, fmt("arr<{}> {}", { i(1), i(0) })),
+    w({ trig = "vec", snippetType = "autosnippet" }, fmt("vector<{}> {}", { i(1), i(0) })),
+    w({ trig = "vi", snippetType = "autosnippet" }, t("vector<int>")),
+    w({ trig = "vl", snippetType = "autosnippet" }, t("vector<long long>")),
+    w({ trig = "vd", snippetType = "autosnippet" }, t("vector<long double>")),
+    w({ trig = "vb", snippetType = "autosnippet" }, t("vector<bool>")),
+    w({ trig = "vs", snippetType = "autosnippet" }, t("vector<string>")),
+    w({ trig = "vc", snippetType = "autosnippet" }, t("vector<char>")),
 
-    s({ trig = "vvi", snippetType = "autosnippet" }, t("vector<vector<int>>")),
-    s({ trig = "vvl", snippetType = "autosnippet" }, t("vector<vector<long long>>")),
-    s({ trig = "vvd", snippetType = "autosnippet" }, t("vector<vector<long double>>")),
-    s({ trig = "vvb", snippetType = "autosnippet" }, t("vector<vector<bool>>")),
-    s({ trig = "vvs", snippetType = "autosnippet" }, t("vector<vector<string>>")),
-    s({ trig = "vvc", snippetType = "autosnippet" }, t("vector<vector<char>>")),
-    s(
+    w({ trig = "vvi", snippetType = "autosnippet" }, t("vector<vector<int>>")),
+    w({ trig = "vvl", snippetType = "autosnippet" }, t("vector<vector<long long>>")),
+    w({ trig = "vvd", snippetType = "autosnippet" }, t("vector<vector<long double>>")),
+    w({ trig = "vvb", snippetType = "autosnippet" }, t("vector<vector<bool>>")),
+    w({ trig = "vvs", snippetType = "autosnippet" }, t("vector<vector<string>>")),
+    w({ trig = "vvc", snippetType = "autosnippet" }, t("vector<vector<char>>")),
+    w(
         { trig = "iff", snippetType = "autosnippet" },
         fmt(
             [[
@@ -100,7 +107,7 @@ ls.add_snippets("cpp", {
             }
         )
     ),
-    s(
+    w(
         { trig = "ife", snippetType = "autosnippet" },
         fmt(
             [[
@@ -117,7 +124,7 @@ ls.add_snippets("cpp", {
             }
         )
     ),
-    s(
+    w(
         { trig = "whilee", snippetType = "autosnippet" },
         fmt(
             [[
@@ -131,7 +138,7 @@ ls.add_snippets("cpp", {
             }
         )
     ),
-    s(
+    w(
         { trig = "forr", snippetType = "autosnippet" },
         fmt(
             [[
@@ -142,7 +149,7 @@ ls.add_snippets("cpp", {
             { i(1, "i"), i(2, "0"), rep(1), i(3, "n"), rep(1), i(0) }
         )
     ),
-    s(
+    w(
         { trig = "for=", snippetType = "autosnippet" },
         fmt(
             [[
@@ -153,7 +160,7 @@ ls.add_snippets("cpp", {
             { i(1, "i"), i(2, "0"), rep(1), i(3, "n"), rep(1), i(0) }
         )
     ),
-    s(
+    w(
         { trig = "roff", snippetType = "autosnippet" },
         fmt(
             [[
@@ -164,7 +171,7 @@ ls.add_snippets("cpp", {
             { i(1, "i"), i(2, "n"), rep(1), i(3, "0"), rep(1), i(0) }
         )
     ),
-    s(
+    w(
         { trig = "rof=", snippetType = "autosnippet" },
         fmt(
             [[
@@ -175,7 +182,7 @@ ls.add_snippets("cpp", {
             { i(1, "i"), i(2, "n"), rep(1), i(3, "0"), rep(1), i(0) }
         )
     ),
-    s(
+    w(
         { trig = "fore", snippetType = "autosnippet" },
         fmt(
             [[
@@ -186,12 +193,12 @@ ls.add_snippets("cpp", {
             { i(1, "e"), i(2, "c"), i(0) }
         )
     ),
-    s({ trig = "all", snippetType = "autosnippet" }, fmt("begin({}), end({}){}", { i(1), rep(1), i(0) })),
-    s({ trig = "rall", snippetType = "autosnippet" }, fmt("rbegin({}), rend({}){}", { i(1), rep(1), i(0) })),
-    s({ trig = "sz", snippetType = "autosnippet" }, fmt("(int) {}.size(){}", { i(1), i(0) })),
+    w({ trig = "all", snippetType = "autosnippet" }, fmt("begin({}), end({}){}", { i(1), rep(1), i(0) })),
+    w({ trig = "rall", snippetType = "autosnippet" }, fmt("rbegin({}), rend({}){}", { i(1), rep(1), i(0) })),
+    w({ trig = "sz", snippetType = "autosnippet" }, fmt("(int) {}.size(){}", { i(1), i(0) })),
 
-    s({ trig = "pb", snippetType = "autosnippet" }, fmt("push_back({});\n{}", { i(1), i(0) })),
-    s({ trig = "epb", snippetType = "autosnippet" }, fmt("emplace_back({});\n{}", { i(1), i(0) })),
-    s({ trig = "ff", snippetType = "autosnippet" }, t("first")),
-    s({ trig = "ss", snippetType = "autosnippet" }, t("second")),
+    w({ trig = "pb", snippetType = "autosnippet" }, fmt("push_back({});\n{}", { i(1), i(0) })),
+    w({ trig = "epb", snippetType = "autosnippet" }, fmt("emplace_back({});\n{}", { i(1), i(0) })),
+    w({ trig = "ff", snippetType = "autosnippet" }, t("first")),
+    w({ trig = "ss", snippetType = "autosnippet" }, t("second")),
 })
