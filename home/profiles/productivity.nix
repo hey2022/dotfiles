a56f9a5e4b43728e2e7370eb3a47b0c43e01f0d0
Yiheng He <yiheng.he@proton.me>
2026-07-01 22:17:53 +0800

    profiles/producitvity/images: init

--- home/profiles/productivity.nix
+++ home/profiles/productivity.nix
@@ -4,23 +4,39 @@
   pkgs,
   ...
 }:
+
+let
+  cfg = config.profiles.productivity;
+in
 {
   options = {
-    profiles.productivity.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Weather to enable productivity profile";
+    profiles.productivity = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Weather to enable productivity profile";
+      };
+      images.enable = lib.mkEnableOption "Photo editing and management software";
     };
   };
-  config = lib.mkIf config.profiles.productivity.enable {
-    programs = {
-      obs-studio.enable = true;
-      pandoc.enable = true;
-      sioyek.enable = true;
-      zathura.enable = false;
-    };
-    home.packages = with pkgs; [
-      quarto
-    ];
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      programs = {
+        obs-studio.enable = true;
+        pandoc.enable = true;
+        sioyek.enable = true;
+        zathura.enable = false;
+      };
+      home.packages = with pkgs; [
+        quarto
+      ];
+    })
+    (lib.mkIf (cfg.enable && cfg.images.enable) {
+      home.packages = with pkgs; [
+        darktable
+        digikam
+        gimp
+      ];
+    })
+  ];
 }
