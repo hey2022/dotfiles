7b92cc9254de2d804a2eeac9348cf43263798c0f
Yiheng He <yiheng.he@proton.me>
2026-07-10 01:34:45 +0800

    patch: Revert "throttled: 0.11 -> 0.12" wlsunset: remove manual package install
    Handled by hm module

--- home/desktop/wayland/wlsunset.nix
+++ home/desktop/wayland/wlsunset.nix
@@ -1,8 +1,4 @@
-{ pkgs, ... }:
-
 {
-  # HACK: https://github.com/nix-community/home-manager/pull/8584
-  home.packages = [ pkgs.wlsunset ];
   services.wlsunset = {
     # Coordinate precision https://xkcd.com/2170/
     latitude = 23.0;
