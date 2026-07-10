c7e9966d2f2f4df54ff094bf4187744ebc9ffa40
Yiheng He <yiheng.he@proton.me>
2026-07-10 18:08:14 +0800

    rofi: runapp if programs.uwsm is enabled

--- home/desktop/launchers/rofi/default.nix
+++ home/desktop/launchers/rofi/default.nix
@@ -1,16 +1,36 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
 {
   programs.rofi = {
-    configPath = "${config.xdg.configHome}/rofi/.hm-config.rasi";
+    theme = lib.mkForce {
+      "@import" = "theme";
+    };
+    extraConfig =
+      let
+        run-prefix = if config.programs.uwsm.enable then "runapp -- " else "";
+      in
+      {
+        modes = "drun,run,recursivebrowser,window";
+        show-icons = true;
+        display-drun = "  ";
+        display-run = "";
+        display-recursivebrowser = "";
+        display-window = "";
+        drun-display-format = "{name}";
+        window-format = "{w}{c}";
+        run-command = "${run-prefix}{cmd}";
+        recursivebrowser = {
+          command = "${run-prefix}xdg-open";
+        };
+      };
     plugins = with pkgs; [
       rofi-calc
       rofi-emoji
     ];
   };
-  xdg.configFile."rofi/config.rasi".source = config.lib.hm.mkFlakeSymlink ./config.rasi;
   xdg.dataFile."rofi/themes/theme.rasi".source = config.lib.hm.mkFlakeSymlink ./theme.rasi;
 }
