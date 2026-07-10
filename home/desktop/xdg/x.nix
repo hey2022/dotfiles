58892c5d51c1e3948dba686c5b8b1ea166a47bd6
Yiheng He <yiheng.he@proton.me>
2026-07-10 23:36:17 +0800

    xresources: add xrdb to path

--- home/desktop/xdg/x.nix
+++ home/desktop/xdg/x.nix
@@ -1,7 +1,8 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   xsession.initExtra = "xrdb -merge ${config.xresources.path}";
   xresources.properties = {
     "Xft.dpi" = 192;
   };
+  home.packages = [ pkgs.xrdb ];
 }
