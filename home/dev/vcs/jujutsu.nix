f0dc10111c34ff0eb2b008637acfb0a875e78ca7
Yiheng He <yiheng.he@proton.me>
2026-07-16 02:22:37 +0800

    jujutsu: enable

--- home/dev/vcs/jujutsu.nix
+++ home/dev/vcs/jujutsu.nix
@@ -1,28 +1,37 @@
-_: {
-  programs.jujutsu = {
-    enable = false;
-    settings = {
-      user = {
-        email = "yiheng.he@proton.me";
-        name = "Yiheng He";
+{
+  programs = {
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          email = "yiheng.he@proton.me";
+          name = "Yiheng He";
+        };
+        signing = {
+          behavior = "own";
+          backend = "gpg";
+        };
+        ui = {
+          editor = "nvim";
+        };
+        aliases = {
+          tug = [
+            "bookmark"
+            "move"
+            "--from"
+            "heads(::@- & bookmarks())"
+            "--to"
+            "@-"
+          ];
+        };
       };
-      signing = {
-        behavior = "own";
-        backend = "gpg";
-      };
-      ui = {
-        editor = "emacsclient -nw";
-      };
-      aliases = {
-        tug = [
-          "bookmark"
-          "move"
-          "--from"
-          "heads(::@- & bookmarks())"
-          "--to"
-          "@-"
-        ];
-      };
+    };
+    jjui = {
+      enable = true;
+    };
+    delta = {
+      enable = true;
+      enableJujutsuIntegration = true;
     };
   };
 }
