b4b7ad7ed2c6cec8d1f2bf2cbbad50669b50a0ec
Yiheng He <yiheng.he@proton.me>
2026-07-17 20:36:06 +0800

    jujutsu: add git-lfs patch

--- home/dev/vcs/jujutsu.nix
+++ home/dev/vcs/jujutsu.nix
@@ -1,4 +1,22 @@
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      jujutsu = prev.jujutsu.overrideAttrs (oldAttrs: rec {
+        # HACK: git-lfs: add ability to ignore files based on git attributes https://github.com/jj-vcs/jj/pull/9635
+        version = "0.42.0";
+        src = prev.fetchFromGitHub {
+          owner = "jj-vcs";
+          repo = "jj";
+          rev = "cf6c9db88f0d10c47515d3eba2aee3f9b3fcb463";
+          hash = "sha256-5HCpqRUbnkmECj9OYZsnfmudHOBCdmzsnnMjH1CU6NY=";
+        };
+        cargoDeps = final.rustPlatform.fetchCargoVendor {
+          inherit src;
+          hash = "sha256-wyaFOMSpJqe4BMHsYFp2+JjUWphpQ64QbE/Am2V4D5A=";
+        };
+      });
+    })
+  ];
   programs = {
     jujutsu = {
       enable = true;
