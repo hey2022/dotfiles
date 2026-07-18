856b84224155201333cc7779f871b32ae5ec4067
Yiheng He <yiheng.he@proton.me>
2026-07-18 13:27:52 +0800

    steam: harden ports

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }:
 {
   programs.steam = {
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
     protontricks.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
