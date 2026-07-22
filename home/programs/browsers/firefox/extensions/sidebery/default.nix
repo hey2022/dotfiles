276642967ebaf24a0dc6f950c66acfefb5a51f9e
Yiheng He <yiheng.he@proton.me>
2026-07-22 23:17:54 +0800

    sidebery: convert school to UIUC

--- home/programs/browsers/firefox/extensions/sidebery/default.nix
+++ home/programs/browsers/firefox/extensions/sidebery/default.nix
@@ -2,14 +2,15 @@
 
 {
   packages = [ pkgs.nur.repos.rycee.firefox-addons.sidebery ];
+  # Can convert json to nix with
+  # nix eval --expr "builtins.fromJSON ''$(wl-paste)''"
   settings."{3c078156-979c-498b-8990-85f7987dd929}".settings = {
     "sidebarCSS" = builtins.readFile ./style.css;
     sidebar = {
       nav = [
         "tabs"
         "dev"
-        "school"
-        "application"
+        "uiuc"
         "add_tp"
         "sp-0"
         "settings"
@@ -29,19 +30,12 @@
           color = "blue";
           iconSVG = "icon_code";
         };
-        school = {
-          name = "school";
-          id = "school";
+        uiuc = {
+          name = "UIUC";
+          id = "uiuc";
           type = 2;
-          color = "purple";
-          iconSVG = "icon_edu";
-        };
-        application = {
-          name = "application";
-          id = "application";
-          type = 2;
-          color = "red";
-          iconSVG = "icon_clipboard";
+          iconIMGSrc = "https://cdn.brand.illinois.edu/favicon.ico";
+          iconIMG = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABAklEQVRIS2NkQALCOt5BvAx/1giz/EIWJpr9+jcbwxcGDs93VzfuhGliRLcgWvDJ2g6FG0Qbiqww564Ow4aPMh54LYgTery2T/E6WRak3dFlWPtBdrhbYMj+cY2n4GtwEPEy/2GIEXvCyMn0D2uQffnLzLDklcz/b/+YwfIb3okzXP0piDuSpXR9NX78+ReKlAJCN6if0bHhf4eSGGDyu96L/I+6bXTuPwPDRpgYy2/WZa/ubLiLNRWhO1NI03v5OrWz4fYCb7FasO2d6P/Y24Zz3t3YmoYrVWDVCFM8agEoJEaDaDQVUTmj0byoANVoNC3sQBaMVjgEq0yaV/rUbrYAAKLNNCgLaVrPAAAAAElFTkSuQmCC";
         };
       };
     };
