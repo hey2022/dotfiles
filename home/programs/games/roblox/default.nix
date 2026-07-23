ed4c264a6dc96feca25086591b38c01ce9d83837
Yiheng He <yiheng.he@proton.me>
2026-07-24 05:12:44 +0800

    profiles/gaming/roblox: init profile

--- home/programs/games/roblox/default.nix
+++ home/programs/games/roblox/default.nix
@@ -1,4 +1,16 @@
-{ ... }:
 {
-  imports = [ ./sober.nix ];
+  config,
+  lib,
+  ...
+}:
+{
+  options.profiles.gaming.roblox = {
+    enable = lib.mkEnableOption "Minecraft gaming support";
+  };
+  config = lib.mkIf (config.profiles.gaming.enable && config.profiles.gaming.roblox.enable) {
+    services.flatpak.packages = [ "org.vinegarhq.Sober" ];
+    home.activation.sober-config = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      install -D ${config.home.dotfiles}/home/programs/games/roblox/config.json ${config.home.homeDirectory}/.var/app/org.vinegarhq.Sober/config/sober/config.json
+    '';
+  };
 }
