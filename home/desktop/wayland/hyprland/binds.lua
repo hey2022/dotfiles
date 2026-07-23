6172a535e51c75632a1c349caa707fe58d60efef
Yiheng He <yiheng.he@proton.me>
2026-07-23 21:58:11 +0800

    hyprland: change lock keybind to be MOD+SHIFT+ESCAPE

--- home/desktop/wayland/hyprland/binds.lua
+++ home/desktop/wayland/hyprland/binds.lua
@@ -7,8 +7,8 @@ bind("RETURN", exec(cfg.terminal, { uwsm = true }))
 bind("SHIFT + RETURN", exec(cfg.terminal .. " nvim", { uwsm = true }))
 bind("ALT + RETURN", exec("neovim-anywhere", { uwsm = true }))
 bind("SHIFT + E", exec("loginctl terminate-session $XDG_SESSION_ID"))
-bind("ESCAPE", exec("loginctl lock-session"))
-bind("SHIFT + ESCAPE", exec("wlogout -p layer-shell", { toggle = true }))
+bind("SHIFT + ESCAPE", exec("loginctl lock-session"))
+bind("CTRL + ESCAPE", exec("wlogout -p layer-shell", { toggle = true }))
 
 -- menu
 bind("SPACE", exec("rofi -show drun", { toggle = true }))
