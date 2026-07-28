377168fcacc33e31bab09ecaa17f6727e07408d2
Yiheng He <yiheng.he@proton.me>
2026-07-29 07:54:05 +0800

    boot: use cachyos kernel

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -6,7 +6,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_cachyos;
     kernel.sysctl = {
       "kernel.sysrq" = 1;
     };
