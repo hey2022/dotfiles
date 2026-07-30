44d78ce63368c6106e2c29304dea245f4f079409
Yiheng He <yiheng.he@proton.me>
2026-07-31 07:35:03 +0800

    hyprland: use upstream flake

--- system/nix.nix
+++ system/nix.nix
@@ -19,10 +19,12 @@
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=20"
         "https://mirrors.ustc.edu.cn/nix-channels/store?priority=20"
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
         "https://cache.nixos.org?priority=100"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
     optimise.automatic = true;
