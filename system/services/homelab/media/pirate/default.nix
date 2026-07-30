44dc06d22c6aa6a857c008b3517fea1231ebbb98
Yiheng He <yiheng.he@proton.me>
2026-07-31 01:34:37 +0800

    pirate: reorganize file structure

---
+++ system/services/homelab/media/pirate/default.nix
@@ -0,0 +1,14 @@
+{
+  imports = [
+    ./prowlarr.nix
+    ./radarr.nix
+    ./sonarr.nix
+    ./lidarr
+    ./bazarr.nix
+    ./jellyfin.nix
+    ./seerr.nix
+    ./navidrome.nix
+    ./qbittorrent.nix
+    ./suwayomi.nix
+  ];
+}
