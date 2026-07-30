4bde3b3b71fabc176087c238fa8130a752c5440d
Yiheng He <yiheng.he@proton.me>
2026-07-31 02:57:49 +0800

    lidarr: absorb beets

--- system/services/homelab/media/pirate/lidarr/beets.nix
+++ system/services/homelab/media/pirate/lidarr/beets.nix
@@ -14,6 +14,39 @@ let
     ];
     text = builtins.readFile ./beets-import.sh;
   };
+  beetsSettings = {
+    directory = "${config.profiles.homelab.mediaDir}/music";
+    library = "/etc/beets/library.db";
+    import.hardlink = true;
+    plugins = [
+      "replaygain"
+      "scrub"
+      "chroma"
+      "fromfilename"
+      "fetchart"
+      "permissions"
+    ];
+
+    replaygain.backend = "ffmpeg";
+
+    ui.color = true;
+
+    paths = {
+      default = "$albumartist/$album/$track $title";
+      singleton = "Singletons/$artist - $title";
+      comp = "$genre/$album/$track $title";
+      "albumtype:soundtrack" = "Soundtracks/$album/$track $title";
+    };
+
+    match = {
+      strong_rec_thresh = 0.10;
+    };
+
+    permissions = {
+      dir = 775;
+      file = 664;
+    };
+  };
 in
 {
   config = lib.mkIf config.services.lidarr.enable {
@@ -21,5 +54,12 @@ in
       mkdir -p /var/lib/lidarr/.config/Lidarr
       cp ${lib.getExe beetsImport} /var/lib/lidarr/.config/Lidarr/beets-import.sh
     '';
+    environment = {
+      systemPackages = with pkgs; [
+        beets
+        ffmpeg
+      ];
+      etc."beets/config.yaml".source = (pkgs.formats.yaml { }).generate "config.yaml" beetsSettings;
+    };
   };
 }
