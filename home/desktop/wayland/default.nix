0542c6262e1a214def5efceb64f1e0aeed696c82
Yiheng He <yiheng.he@proton.me>
2026-08-01 00:03:24 +0800

    desktop/wayland: use import-tree

--- home/desktop/wayland/default.nix
+++ home/desktop/wayland/default.nix
@@ -1,22 +1,5 @@
 { pkgs, ... }:
 {
-  imports = [
-    ./espanso
-    ./hypridle.nix
-    ./hyprland
-    ./hyprlock
-    ./hyprsunset.nix
-    ./niri
-    ./quickshell
-    ./swaylock.nix
-    ./swaync.nix
-    ./uwsm.nix
-    ./waybar
-    ./wayscriber.nix
-    ./wlogout.nix
-    ./wlsunset.nix
-    ./wluma.nix
-  ];
   home.packages = with pkgs; [
     brightnessctl
     grim
