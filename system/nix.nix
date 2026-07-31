6361cdd6016e12926712ae11f01c27a74ac040ee
Yiheng He <yiheng.he@proton.me>
2026-07-31 10:17:38 +0800

    nix: use harmonia cache

--- system/nix.nix
+++ system/nix.nix
@@ -20,11 +20,13 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store?priority=20"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://cache.hey2022.dev"
         "https://cache.nixos.org?priority=100"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.hey2022.dev:jjEoh4E8SFmnwrDvEPQRF3aTj/Y4iNzWj6PkdlBAyGY="
       ];
     };
     optimise.automatic = true;
