54d89ba2893742d9eb3346b8598928983497bbbd
Yiheng He <yiheng.he@proton.me>
2026-08-01 08:54:13 +0800

    vcs: let git and jujutsu be togglable

--- home/dev/vcs/git.nix
+++ home/dev/vcs/git.nix
@@ -1,71 +1,76 @@
+{ config, lib, ... }:
+let
+  cfg = config.programs.git;
+in
 {
-  programs = {
-    git = {
-      enable = true;
-      lfs.enable = true;
-      includes = [ { path = "~/.config/git/maintenance"; } ];
-      settings = {
-        aliases = {
-          maintain = "maintenance register --config-file $HOME/.config/git/maintenance";
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      programs = {
+        git = {
+          lfs.enable = true;
+          includes = [ { path = "~/.config/git/maintenance"; } ];
+          settings = {
+            aliases = {
+              maintain = "maintenance register --config-file $HOME/.config/git/maintenance";
+            };
+            user = {
+              email = "yiheng.he@proton.me";
+              name = "Yiheng He";
+            };
+            branch.sort = "-committerdate";
+            column.ui = "auto";
+            core = {
+              fsmonitor = true;
+              untrackedcache = true;
+              editor = "nvim";
+            };
+            diff = {
+              algorithm = "histogram";
+              colorMoved = "plain";
+              mnemonicPrefix = true;
+              renames = true;
+            };
+            fetch = {
+              all = true;
+              prune = true;
+              pruneTags = true;
+              writeCommitGraph = true;
+            };
+            help.autocorrect = "prompt";
+            init.defaultBranch = "main";
+            merge = {
+              conflictstyle = "zdiff3";
+            };
+            pull.rebase = true;
+            push = {
+              autoSetupRemote = true;
+              followTags = true;
+            };
+            rebase = {
+              autoSquash = true;
+              autoStash = true;
+              updateRefs = true;
+            };
+            rerere = {
+              enabled = true;
+              autoupdate = true;
+            };
+            tag.sort = "version:refname";
+          };
+          signing = {
+            signByDefault = true;
+          };
         };
-        user = {
-          email = "yiheng.he@proton.me";
-          name = "Yiheng He";
+        lazygit = {
+          enable = true;
         };
-        branch.sort = "-committerdate";
-        column.ui = "auto";
-        core = {
-          fsmonitor = true;
-          untrackedcache = true;
-          editor = "nvim";
-        };
-        diff = {
-          algorithm = "histogram";
-          colorMoved = "plain";
-          mnemonicPrefix = true;
-          renames = true;
-        };
-        fetch = {
-          all = true;
-          prune = true;
-          pruneTags = true;
-          writeCommitGraph = true;
-        };
-        help.autocorrect = "prompt";
-        init.defaultBranch = "main";
-        merge = {
-          conflictstyle = "zdiff3";
-        };
-        pull.rebase = true;
-        push = {
-          autoSetupRemote = true;
-          followTags = true;
-        };
-        rebase = {
-          autoSquash = true;
-          autoStash = true;
-          updateRefs = true;
-        };
-        rerere = {
-          enabled = true;
-          autoupdate = true;
-        };
-        tag.sort = "version:refname";
       };
-      signing = {
-        signByDefault = true;
+    })
+    {
+      programs = {
+        difftastic.git.enable = true;
+        mergiraf.enableGitIntegration = true;
       };
-    };
-    lazygit = {
-      enable = true;
-    };
-    difftastic = {
-      enable = true;
-      git.enable = true;
-    };
-    mergiraf = {
-      enable = true;
-      enableGitIntegration = true;
-    };
-  };
+    }
+  ];
 }
--- home/dev/vcs/jujutsu.nix
+++ home/dev/vcs/jujutsu.nix
@@ -1,88 +1,93 @@
+{ config, lib, ... }:
+let
+  cfg = config.programs.jujutsu;
+in
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      jujutsu = prev.jujutsu.overrideAttrs rec {
-        # HACK: git-lfs: add ability to ignore files based on git attributes https://github.com/jj-vcs/jj/pull/9635
-        version = "0.43.0";
-        src = prev.fetchFromGitHub {
-          owner = "jj-vcs";
-          repo = "jj";
-          rev = "263ba237ff13ca4b85dc490aee797713b990810e";
-          hash = "sha256-c7uv+5b/n+WphNXPKHu5Ta+n19WcP7r1kBvfBLrQ1A8=";
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      nixpkgs.overlays = [
+        (final: prev: {
+          jujutsu = prev.jujutsu.overrideAttrs rec {
+            # HACK: git-lfs: add ability to ignore files based on git attributes https://github.com/jj-vcs/jj/pull/9635
+            version = "0.43.0";
+            src = prev.fetchFromGitHub {
+              owner = "jj-vcs";
+              repo = "jj";
+              rev = "263ba237ff13ca4b85dc490aee797713b990810e";
+              hash = "sha256-c7uv+5b/n+WphNXPKHu5Ta+n19WcP7r1kBvfBLrQ1A8=";
+            };
+            cargoDeps = final.rustPlatform.fetchCargoVendor {
+              inherit src;
+              hash = "sha256-wP3u+kxVfu1bYEKtHq5eqSoiPAFSyEXagHo7Xj8rzys=";
+            };
+          };
+        })
+      ];
+      programs = {
+        jujutsu = {
+          settings = {
+            user = {
+              email = "yiheng.he@proton.me";
+              name = "Yiheng He";
+            };
+            signing = {
+              behavior = "own";
+              backend = "gpg";
+            };
+            ui = {
+              editor = "nvim";
+            };
+            revsets = {
+              log = "all()";
+            };
+            revset-aliases = {
+              "closest_merge(to)" = "heads(::to & merges())";
+            };
+            aliases = {
+              tug = [
+                "bookmark"
+                "move"
+                "--from"
+                "heads(::@- & bookmarks())"
+                "--to"
+                "@-"
+              ];
+              # https://isaaccorbrey.com/notes/jujutsu-megamerges-for-fun-and-profit
+              stack = [
+                "rebase"
+                "--after"
+                "trunk()"
+                "--before"
+                "closest_merge(@)"
+                "--revision"
+              ];
+              # https://isaaccorbrey.com/notes/jujutsu-megamerges-for-fun-and-profit
+              stage = [
+                "stack"
+                "closest_merge(@)+:: ~ empty()"
+              ];
+              # https://isaaccorbrey.com/notes/jujutsu-megamerges-for-fun-and-profit
+              restack = [
+                "rebase"
+                "--onto"
+                "trunk()"
+                "--source"
+                "roots(trunk()..) & mutable()"
+                "--simplify-parents"
+              ];
+            };
+          };
         };
-        cargoDeps = final.rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-wP3u+kxVfu1bYEKtHq5eqSoiPAFSyEXagHo7Xj8rzys=";
+        jjui = {
+          enable = true;
         };
       };
     })
-  ];
-  programs = {
-    jujutsu = {
-      enable = true;
-      settings = {
-        user = {
-          email = "yiheng.he@proton.me";
-          name = "Yiheng He";
-        };
-        signing = {
-          behavior = "own";
-          backend = "gpg";
-        };
-        ui = {
-          editor = "nvim";
-        };
-        revsets = {
-          log = "all()";
-        };
-        revset-aliases = {
-          "closest_merge(to)" = "heads(::to & merges())";
-        };
-        aliases = {
-          tug = [
-            "bookmark"
-            "move"
-            "--from"
-            "heads(::@- & bookmarks())"
-            "--to"
-            "@-"
-          ];
-          # https://isaaccorbrey.com/notes/jujutsu-megamerges-for-fun-and-profit
-          stack = [
-            "rebase"
-            "--after"
-            "trunk()"
-            "--before"
-            "closest_merge(@)"
-            "--revision"
-          ];
-          # https://isaaccorbrey.com/notes/jujutsu-megamerges-for-fun-and-profit
-          stage = [
-            "stack"
-            "closest_merge(@)+:: ~ empty()"
-          ];
-          # https://isaaccorbrey.com/notes/jujutsu-megamerges-for-fun-and-profit
-          restack = [
-            "rebase"
-            "--onto"
-            "trunk()"
-            "--source"
-            "roots(trunk()..) & mutable()"
-            "--simplify-parents"
-          ];
-        };
+    {
+      programs = {
+        difftastic.jujutsu.enable = true;
+        mergiraf.enableJujutsuIntegration = true;
       };
-    };
-    jjui = {
-      enable = true;
-    };
-    difftastic = {
-      enable = true;
-      jujutsu.enable = true;
-    };
-    mergiraf = {
-      enable = true;
-      enableJujutsuIntegration = true;
-    };
-  };
+    }
+  ];
 }
