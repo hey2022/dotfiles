8f3666e3b14faeedadb92b22f9c7cfe9ac0ed04e
Yiheng He <yiheng.he@proton.me>
2026-08-02 00:42:43 +0800

    flake/git-hooks: setup pre-push hooks with jujutsu

--- flake-modules/git-hooks.nix
+++ flake-modules/git-hooks.nix
@@ -7,10 +7,20 @@
     {
       pre-commit.settings = {
         package = pkgs.prek;
+        default_stages = [
+          "pre-commit"
+          "pre-push"
+        ];
         hooks = {
           treefmt.enable = true;
         };
       };
-      devShells.default = config.pre-commit.devShell;
+      devShells.default = pkgs.mkShell {
+        shellHook = ''
+          ${config.pre-commit.shellHook}
+          export JJ_PRE_PUSH_CHECKER=prek
+        '';
+        packages = config.pre-commit.settings.enabledPackages;
+      };
     };
 }
