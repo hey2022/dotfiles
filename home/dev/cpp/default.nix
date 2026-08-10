3154f4b90d9ed563d7309051b50af5357e6338fc
Yiheng He <yiheng.he@proton.me>
2026-08-10 09:15:41 +0800

    dev/cpp: reduce tools

--- home/dev/cpp/default.nix
+++ home/dev/cpp/default.nix
@@ -1,15 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (lib.hiPrio clang)
-    clang-tools
-    cmake
-    conan
-    cppcheck
-    gcc
     mold
     ninja
-    vcpkg
   ];
   home.file.".conan2/profiles/default".source = ./conan-profile;
 }
