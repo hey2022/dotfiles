495b76a696fc851683fb887da6b175ba27afaada
Yiheng He <yiheng.he@proton.me>
2026-08-10 09:20:07 +0800

    dev/rust: reduce tools

--- home/dev/rust/default.nix
+++ home/dev/rust/default.nix
@@ -6,14 +6,8 @@
 {
   home = {
     packages = with pkgs; [
-      cargo
-      cargo-update
       mold
-      rust-analyzer
-      rustc
-      cargo-binstall
     ];
     file.".cargo/config.toml".source = config.lib.hm.mkFlakeSymlink ./config.toml;
-    sessionPath = [ "$HOME/.cargo/bin" ];
   };
 }
