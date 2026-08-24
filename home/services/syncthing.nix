b1c86fbedb92053d4afd36b2a2b8a414f53d76da
Yiheng He <yiheng.he@proton.me>
2026-08-24 07:39:55 -0500

    syncthing: add config location path

--- home/services/syncthing.nix
+++ home/services/syncthing.nix
@@ -1,4 +1,7 @@
-_: {
+{
+  # Config location: ~/.local/state/syncthing/config.xml
+  # Used for changing sync folder location
+  # Stop syncthing, edit the location, then restart
   services.syncthing = {
     enable = true;
     overrideDevices = false;
