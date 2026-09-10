d8892fb833cff718039338ccedbc4dae70e2e03c
Yiheng He <yiheng.he@proton.me>
2026-09-10 16:16:53 -0500

    tablet: fix udev rules

--- system/hardware/tablet.nix
+++ system/hardware/tablet.nix
@@ -1,4 +1,9 @@
-{ pkgs-local, ... }:
+{
+  pkgs,
+  pkgs-local,
+  lib,
+  ...
+}:
 {
   # HACK: https://github.com/OpenTabletDriver/OpenTabletDriver/issues/2118 running custom build of OTD
   hardware = {
@@ -9,7 +14,29 @@
     uinput.enable = true;
   };
   boot.kernelModules = [ "uinput" ];
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", KERNELS=="0005:056A:03C8.*", MODE="0666"
-  '';
+  services.udev.packages = lib.singleton (
+    pkgs.writeTextFile {
+      name = "otd-rules";
+      text = ''
+        # Wacom CTL-6100WL
+        KERNEL=="hidraw*", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="0378", TAG+="uaccess", TAG+="udev-acl"
+        KERNEL=="hidraw*", KERNELS=="0005:056A:0378.*", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="0378", TAG+="uaccess", TAG+="udev-acl"
+        # Wacom CTL-6100WL
+        KERNEL=="hidraw*", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="0379", TAG+="uaccess", TAG+="udev-acl"
+        KERNEL=="hidraw*", KERNELS=="0005:056A:0379.*", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="0379", TAG+="uaccess", TAG+="udev-acl"
+
+        # Wacom CTL-6100WL
+        KERNEL=="hidraw*", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="03c7", TAG+="uaccess", TAG+="udev-acl"
+        KERNEL=="hidraw*", KERNELS=="0005:056A:03C7.*", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="03c7", TAG+="uaccess", TAG+="udev-acl"
+        # Wacom CTL-6100WL
+        KERNEL=="hidraw*", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="03c8", TAG+="uaccess", TAG+="udev-acl"
+        KERNEL=="hidraw*", KERNELS=="0005:056A:03C8.*", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="056a", ATTRS{idProduct}=="03c8", TAG+="uaccess", TAG+="udev-acl"
+      '';
+      destination = "/etc/udev/rules.d/70-otd.rules";
+    }
+  );
 }
