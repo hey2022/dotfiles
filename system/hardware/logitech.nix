6d6eb5a187fc9c5e8384ebfc2c2fbaaf7b79f3ce
Yiheng He <yiheng.he@proton.me>
2026-09-13 18:49:35 -0500

    hardware/logitech: rename enableGraphical to solaar

--- system/hardware/logitech.nix
+++ system/hardware/logitech.nix
@@ -1,7 +1,7 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
-  hardware.logitech.wireless = {
-    enable = config.host.desktop;
-    enableGraphical = true;
+  config = lib.mkIf config.host.desktop {
+    hardware.logitech.wireless.enable = true;
+    programs.solaar.enable = true;
   };
 }
